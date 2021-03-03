e3bca655a1c7a501e95d32249f8ba5b0658a67cd
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-03-03 18:07:05 +0200

    Auto commit

--- train_analysis/GC_test_shoes_usage_duration.R
+++ train_analysis/GC_test_shoes_usage_duration.R
@@ -28,7 +28,12 @@ metrics <- readRDS("~/LOGs/GCmetrics.Rds")
 ####  Copy for GC below  ####################################################
 
 library(data.table)
-library(randomcoloR)
+
+
+# This crushes version 3.6-DRV2006 of goldenchretah
+# library(randomcoloR)
+
+cols <- c("#f22e2e", "#d9b629", "#30ff83", "#3083ff", "#f22ee5", "#33161e", "#e6a89e", "#bbbf84", "#1d995f", "#324473", "#cc8dc8", "#59111b", "#b25c22", "#b1f22e", "#9ee6d7", "#482ef2", "#66465b", "#33200a", "#385911", "#24a0bf", "#270f4d", "#731647", "#664a13", "#414d35", "#22444d", "#6b1880", "#ff70a9")
 
 metrics <- data.table(metrics)
 gdata::write.fwf(metrics[, .(Dist = round(sum(Distance),1),
@@ -106,7 +111,7 @@ plot(1, type="n",
 
 ## create color palete
 n      <- length(unique(gather$Shoes))
-cols   <- distinctColorPalette(n)
+# cols   <- distinctColorPalette(n)
 
 ## add lines to plot
 sn   <- c()
