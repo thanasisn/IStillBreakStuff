9a7e08d5dbd26df3479f0870ac5f98f91d985319
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-05-11 09:13:01 +0300

    Auto commit

--- gpx_tools/etrex_sync.sh
+++ gpx_tools/etrex_sync.sh
@@ -22,9 +22,10 @@ else
 fi
 
 ## copy some of regular waypoints to the device
-cp -vu "$HOME/LOGs/waypoints_etrex/wpt_Seix Sou.gpx" "/media/athan/GARMIN/Garmin/GPX"
+cp -vu "$HOME/LOGs/waypoints_etrex/wpt_Seix Sou.gpx"                "/media/athan/GARMIN/Garmin/GPX"
 cp -vu "$HOME/LOGs/waypoints_etrex/wpt_Thessaloniki-Stavros-O4.gpx" "/media/athan/GARMIN/Garmin/GPX"
-cp -vu "$HOME/LOGs/waypoints_etrex/wpt_Xortiatis.gpx" "/media/athan/GARMIN/Garmin/GPX"
+cp -vu "$HOME/LOGs/waypoints_etrex/wpt_Xortiatis.gpx"               "/media/athan/GARMIN/Garmin/GPX"
+cp -vu "$HOME/LOGs/waypoints_etrex/wpt_Asprobalta.gpx"              "/media/athan/GARMIN/Garmin/GPX"
 
 ## check sources and sync
 if [ -d "$SOURCE2" ]; then
