7828adf8fe8f9dc5fedd62988e14b4d87c6dc9af
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-06-25 12:08:31 +0300

    Auto commit

--- data_tools/scrap_car_en.sh
+++ data_tools/scrap_car_en.sh
@@ -4,7 +4,7 @@
 #### Scrap data from car.gr for processing
 
 
-REPO="$HOME/LOGs/car_gr_repo"
+REPO="$HOME/LOGs/car_en_remp"
 mkdir -p "$REPO"
 
 
@@ -13,7 +13,7 @@ for pp in {1..30}; do
     echo "PAGE $pp"
 
     ## search term
-    url="https://www.car.gr/classifieds/cars/?lang=el&fs=1&condition=used&offer_type=sale&price-from=%3E4000&price-to=%3C17000&mileage-to=%3C200000&drive_type=4x4&engine_size-to=2000&euroclass=5&euroclass=6&euroclass=7&euroclass=9&pg=$pp"
+    url="https://www.car.gr/classifieds/cars/?lang=en&fs=1&condition=used&offer_type=sale&price-from=%3E4000&price-to=%3C17000&mileage-to=%3C200000&drive_type=4x4&engine_size-to=2000&euroclass=5&euroclass=6&euroclass=7&euroclass=9&pg=$pp"
 
     ## Diesel: &fuel_type=2
 
@@ -44,6 +44,8 @@ for pp in {1..30}; do
     ## get all search results
     echo "$downlist" | while read line; do
 
+        line="${line}?lang=en"
+
         targetfile="$(basename $line)"
         newfile="${REPO}/${targetfile}_$(date +%F).txt"
 
@@ -62,29 +64,30 @@ for pp in {1..30}; do
                      -nolist                 \
                      "$line")"
 
+       # echo "$POST"
 
         ## store data for later
         (
         echo "CAPTURE DATE::$(date +%F_%T)"
         echo "$POST" |\
-            sed '1,/  Σύνδεση Δωρεάν Καταχώρηση/d'        |\
-            sed '/Παρόμοιες αναζητήσεις/,$d'              |\
-            sed '/^$/d'                                   |\
-            sed '/(BUTTON)/d'                             |\
-            sed '/\[svg+xml.*/d'                          |\
-            sed 's/[_]\+/_/g'                             |\
-            sed '/[ ]\+[A-Za-z0-9]\+=]/d'                 |\
-            sed '/Μη στέλνετε προκαταβολή αν δεν έχετε/d' |\
+            sed '1,/  Login Free Classified/d'             |\
+            sed '/Similar searches/,$d'                    |\
+            sed '/^$/d'                                    |\
+            sed '/(BUTTON)/d'                              |\
+            sed '/\[svg+xml.*/d'                           |\
+            sed 's/[_]\+/_/g'                              |\
+            sed '/[ ]\+[A-Za-z0-9]\+=]/d'                  |\
+            sed '/Do not send a downpayment if you have/d' |\
             sed '/-thumb-[0-9]\+/d'
-        ) | iconv -c -f utf-8 -t utf-8 > "$newfile"
+        ) | iconv -c -f utf-8 -t utf-8  > "$newfile"
 
         ## be nice
         sleep 10s
     done
 done
 
-echo "Run file parsing"
-"$HOME/CODE/data_tools/parse_car_gr.R"
+# echo "Run file parsing"
+# "$HOME/CODE/data_tools/parse_car_gr.R"
 
 ## end coding
 exit 0
