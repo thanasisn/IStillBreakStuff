032a660913f0cf139dd397a98dfe12c5f6e8235c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-09-26 11:42:22 +0300

    Auto commit

--- system_tools/compress_files_best.sh
+++ system_tools/compress_files_best.sh
@@ -3,63 +3,129 @@
 
 #### Compress individual files after testing for best compression method for each file
 
-## Use glob to pass files. Example: compress_file_best.sh ./**/*.dat
-## Will ignore folders
 ## The intend is to be used for archiving original data like .csv .dat .txt
-## Most programming languages can read this files directly
-## Will try to compress already compressed files
-## Will not recompress with the same algorithm
+## Most programming languages can read this files directly anyway
+## Can be run interactively or in batch mode for scripting
 
-##TODO add a threshold check on compression ratio or size reduction
+## Defaults ##
+SHOW_TABLE="yes"
+APPLY_COMPRESSION="yes"
+REMOVE_ORIGINAL="yes"
+INTERACTIVE="yes"
+BYTES_REDUCTION="10"
+OVERWRITE="yes"
 
-# SHOW_TABLE=true
-APPLY_COMPRESSION=true
-REMOVE_ORIGINAL=true
-
-##TODO don't compress if size reduction in bytes is less than this
-SIZE_THRESHOLD=10
-
-##TODO don't compress if ration reduction in % is less than this
-RATIO_THRESHOLD=0.01
-
-
-
-## compression commands to test
 ALGO=( bzip2 gzip xz )
 
+function _usage()
+{
+cat <<EOF
+
+$*
+
+    Usage: $(basename "${0}") <[options]> ./Glob/path/**/*.*
+
+    Options:
+        --ask-human       ($INTERACTIVE) Ask human for input. Setting to no may be dangerous.
+        --compress        ($APPLY_COMPRESSION) Write file with the best compressed. If no just test for best algorithm.
+        --help            Show this message and exit.
+        --remove-source   ($REMOVE_ORIGINAL) Remove source file if compression was successful.
+        --show-table      ($SHOW_TABLE) Show stats table for all tests
+        --threshold-bytes ($BYTES_REDUCTION) Don't compress if benefit is less than $BYTES_REDUCTION bytes
+        --overwrite       ($OVERWRITE) Overwrite existing files
+
+    Notes:
+        Will try  ${ALGO[@]}  compressions with all levels add will stop when it finds the best.
+        Will ignore folders.
+        Will not try to compress an already compressed file.
+
+EOF
+}
+
+ARGUMENT_LIST=(
+    "ask-human"
+    "compress"
+    "help"
+    "remove-source"
+    "show-table"
+    "overwrite"
+    "threshold-bytes"
+)
+
+# read arguments
+opts=$(getopt \
+    --longoptions "$(printf "%s:," "${ARGUMENT_LIST[@]}")" \
+    --name "$(basename "$0")" \
+    --options "" \
+    -- "$@"
+)
+
+eval set --$opts
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --show-table )     SHOW_TABLE="$2";        shift 2 ;;
+        --compress )       APPLY_COMPRESSION="$2"; shift 2 ;;
+        --remove-source )  REMOVE_ORIGINAL="$2";   shift 2 ;;
+        --ask-human )      INTERACTIVE="$2";       shift 2 ;;
+        --overwrite )      OVERWRITE="$2";         shift 2 ;;
+        --threshold-bytes) BYTES_REDUCTION="$2";   shift 2
+                            if [ "$BYTES_REDUCTION" -eq "$BYTES_REDUCTION" ] 2>/dev/null; then
+                                : #echo "$BYTES_REDUCTION : is a number"
+                            else
+                                _usage " >>> threshold-bytes: $BYTES_REDUCTION NOT A NUMBER <<< "&& exit
+                            fi ;;
+        --help )          _usage && exit ;;
+        -- ) shift ;;
+        * ) break ;;
+    esac
+done
+[ $# = 0 ] && _usage " >>> NO TARGET GIVEN <<<  " && exit
 
 echo
 echo "$@"
 echo
-echo "THE ABOVE FILES WILL BE PROCESSED!"
+echo "THE ABOVE PATHS WILL BE PROCESSED!"
 echo "SHOW TABLE:         $SHOW_TABLE"
 echo "APPLY_COMPRESSION:  $APPLY_COMPRESSION"
 echo "REMOVE_ORIGINAL:    $REMOVE_ORIGINAL"
+echo "ASK HUMAN:          $INTERACTIVE"
+echo "BYTES GAIN LIMIT:   $BYTES_REDUCTION"
+echo "OVERWRITE:          $OVERWRITE"
 echo
-read -p "Are you sure? " -n 1 -r
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo ""
-else
-    echo "EXIT"
-    exit 0
-fi
-
-if [[ $REMOVE_ORIGINAL ]]; then
-    echo
-    echo "************************"
-    echo "    LAST WARNING  !!    "
-    echo " This will remove files "
-    echo "************************"
-    echo
-    read -p "ARE YOUT SURE? "  -r
+## to enter interactive mode
+if [[ ! "$INTERACTIVE" == "no" ]]; then
+    read -p "Are you sure? " -n 1 -r
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         echo ""
     else
         echo "EXIT"
         exit 0
     fi
+
+    if [[ $REMOVE_ORIGINAL == "yes" ]]; then
+        echo
+        echo "************************"
+        echo "    LAST WARNING  !!    "
+        echo " This will remove files "
+        echo "************************"
+        echo
+        read -p "ARE YOU SURE? "  -r
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            echo ""
+        else
+            echo "EXIT"
+            exit 0
+        fi
+    fi
+    else
+    echo "RUNNING IN NON INTERACTIVE MODE!"
+    echo
 fi
 
+
+
+## MAIN LOGIC ##
 
 for af in "$@" ; do
     [[ ! -f "$af" ]] && echo "NOT A FILE: $af" && continue
@@ -70,28 +136,24 @@ for af in "$@" ; do
     codecs=()
     clevel=()
     cratio=()
-
-    echo "$FILESIZE  $af"
+    echo "--------------------------------------------------"
+    echo "SIZE:     $FILESIZE  $af"
 
     ## compression commands to test
     for com in "${ALGO[@]}"; do
         bsize=$FILESIZE
         ## compression levels to test
         for cl in {1..9}; do
+            ## test compression command
             size=$($com -c "$af" -"$cl" | wc -c)
-            # echo $com $cl $size
-
-            ## stop when no improvement
-            # [[ $size -ge $bsize ]] && echo "stop $com loop no improvement" && break
+            ## stop when no further improvement
             [[ $size -ge $bsize ]] && break
-
-            ## keep stats
+            ## keep stats in arrays
             codecs+=( "$com"  )
             fsizes+=( "$size" )
             clevel+=( "$cl"   )
-            cratio+=( "$(echo "scale=3; 100 * ($size - $FILESIZE)  / $FILESIZE" | bc | sed -e 's/^-\./-0./' -e 's/^\./0./')" )
-
-            ## remember previous
+            cratio+=( "$(echo "scale=3; 100*($size - $FILESIZE)/$FILESIZE" | bc | sed -e 's/^-\./-0./' -e 's/^\./0./')" )
+            ## remember previous values
             bsize=$size
         done
     done
@@ -100,7 +162,7 @@ for af in "$@" ; do
     indexes=$( for k in "${!fsizes[@]}"; do
                     echo "$k" ' - ' "${fsizes["$k"]}"
                 done | sort -n -k3 | cut -d' ' -f1 | tr '\n' ' ' | sed 's/,$//')
-    ## sorted arrays
+    ## sort arrays with indexes
     Sfsizes=()
     Scodecs=()
     Sclevel=()
@@ -112,52 +174,57 @@ for af in "$@" ; do
         Scratio+=( "${cratio[$i]}" )
     done
 
-    ## just show stats table
-    if [ $SHOW_TABLE ]; then
+    ## print stats table
+    if [[ $SHOW_TABLE == "yes" ]]; then
         paste <(printf "%s\n" "${Scodecs[@]}") <(printf "%s\n" "${Sclevel[@]}") <(printf "%s\n" "${Sfsizes[@]}") <(printf "%s %%\n" "${Scratio[@]}")
     fi
 
     ## compression logic
     if [ ${#Scodecs[@]} -eq 0 ]; then
-        echo "No compression benefits found for file"
-    else
-        ext="$(echo "${Scodecs[0]}" | sed 's/ip//g')"
-        ## usefull to gather stats for multiple files
-        echo "BEST found:        ${Scodecs[0]} ${Sclevel[0]} ${Sfsizes[0]} ${Scratio[0]}% $af "
+        echo "SKIP:     No compression benefits found:  $af"
+        continue
+    fi
 
-        ## skip if file has the same extension
-        fname="$(basename "$af")"
-        oldexten="${fname##*.}"
+    ext="$(echo "${Scodecs[0]}" | sed 's/ip//g')"
+    ## useful to gather stats for multiple files compression analysis
+    echo "BEST:     ${Scodecs[0]} ${Sclevel[0]} ${Sfsizes[0]} ${Scratio[0]}% $af "
+    echo "BENEFIT:  $(( FILESIZE - Sfsizes[0] )) b"
 
-        ## just avoid recompress with the same algorithm
-        if [ "$oldexten" == "$ext" ]; then
-            echo "Same extensions $oldexten $ext"
-            echo "Skip compression!!"
-        else
-            newfile="${af}.${ext}"
-            ## apply best compression to file
-            if [ $APPLY_COMPRESSION ]; then
-                echo "APPLY COMPRESSION: ${Scodecs[0]} -c -${Sclevel[0]} $af > $newfile "
-                ${Scodecs[0]} -c -"${Sclevel[0]}" "$af" > "$newfile"
-                status=$?
-                newsize=$(stat -c%s "$newfile")
-                ## remove original file
-                if [ $newsize -gt 0 ]; then
-                    if [ $REMOVE_ORIGINAL ]; then
-                        [[ $status -eq 0 ]] && trash "$af"
-                        echo "Removed:           $af"
-                    fi
-                else
-                   echo "WARNING empty file: $newfile"
-                fi
+    ## check benefit
+    if [[ $(( FILESIZE - ${Sfsizes[0]} )) -lt $BYTES_REDUCTION  ]]; then
+        echo "SKIP:     Benefit limit exceeded:  $af"
+        continue
+    fi
+
+    ## avoid recompress a compressed file
+    if ( file "$af" | grep -q compressed ) ;then
+        echo "SKIP:    Source already compressed:   $af"
+        continue
+    fi
+
+    newfile="${af}.${ext}"
+    ## apply best compression to file
+    if [[ $APPLY_COMPRESSION == "yes" ]]; then
+        ## avoid overwrite
+        if [[ $OVERWRITE == "no" ]] && [[ -e "$newfile" ]] ; then
+            echo "SKIP:     File exist:  $newfile"
+            continue
+        fi
+        echo "COMPRESS: ${Scodecs[0]} -c -${Sclevel[0]} $af > $newfile "
+        ${Scodecs[0]} -c -"${Sclevel[0]}" "$af" > "$newfile"
+        status=$?
+        newsize=$(stat -c%s "$newfile")
+        ## remove original file if new non empty
+        if [ "$newsize" -gt 0 ]; then
+            if [[  $REMOVE_ORIGINAL == "yes" ]]; then
+                [[ $status -eq 0 ]] && trash "$af"
+                echo "REMOVED:  $af"
             fi
+        else
+            echo "WARNING:  Empty file:  $newfile"
         fi
     fi
-    echo
 done
-
 echo
 echo "FINISHED"
 echo
-exit
-
