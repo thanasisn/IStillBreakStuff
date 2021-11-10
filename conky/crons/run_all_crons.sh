03dc9fc77bedbf4c74934a9422462ee8fa3cd17d
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-11-10 12:13:01 +0200

    Auto commit

--- conky/crons/run_all_crons.sh
+++ conky/crons/run_all_crons.sh
@@ -14,7 +14,7 @@ nice -n 19 ionice -c2 -n7 "${BASEPATH}cron_period_30.sh" &
 sleep 10
 nice -n 19 ionice -c2 -n7 "${BASEPATH}cron_period_90.sh" &
 
-## keep only last n lines of status files
+## keep only last n lines of all status files
 find "/home/athan/LOGs/SYSTEM_LOGS/" -iname "*.status" | while read line; do
     echo "$(tail -2000 $line)" > "$line"
 done
