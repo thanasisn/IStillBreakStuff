ca473962cf00ad39a25fb71708e307ec1aa9bc50
thanasisn <11601717+thanasisn@users.noreply.github.com>
2021-11-17 21:07:05 +0200

    Auto commit

---
+++ system_tools/list_files_by_date.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+## created on 2021-11-17
+
+#### List file by date 
+## This accepts multiple folders
+
+FOLDER="$@"
+
+find $FOLDER -type f -print0           |\
+    xargs -0 stat --format '%Y :%y %n' |\
+    sort -n                            |\
+    cut -d: -f2-
+
+exit 0 
