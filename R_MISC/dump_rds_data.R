d9b1194e2e8a0ce4fec96557ed8277e7c87136a0
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-07-08 20:07:02 +0300

    Auto commit

---
+++ R_MISC/dump_rds_data.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+# /* Copyright (C) 2022 Athanasios Natsis <natsisthanasis@gmail.com> */
+
+rm(list = (ls()[ls() != ""]))
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+
+library(data.table, quietly = T, warn.conflicts = F)
+library(optparse,   quietly = T, warn.conflicts = F)
+
+
+
+option_list <-  list(
+    make_option(c("-o", "--output"),
+                type    = "character",
+                default = "csv",
+                help    = paste0("export file type"),
+                metavar = "yyyy-mm-dd")
+)
+opt_parser <- OptionParser(option_list = option_list)
+args       <- parse_args(opt_parser)
+
+
+## read file
+## dump data
+## export to file formats
+##
+##
+##
