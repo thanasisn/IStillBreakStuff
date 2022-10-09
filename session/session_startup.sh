ffc1de560309def94fc8f0284ae1885275ffe71d
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-10-09 19:07:03 +0300

    Commit 2022-10-09 19:07

--- session/session_startup.sh
+++ session/session_startup.sh
@@ -102,6 +102,13 @@ export DISPLAY=:0 ; $HOME/CODE/conky/scripts/update_background.sh &
 ## start all conky cron scripts and truncate status logs
 $HOME/CODE/conky/crons/run_all_crons.sh &
 
+## test polybar
+killall polybar
+xrandr --listactivemonitors | sed 's/^.* //' | sed 1d | while read screen; do
+    export MONITOR=$screen
+    polybar -c $HOME/.config/polybar/config -r example &
+done
+
 
 
 # zeitgeist-daemon --quit
