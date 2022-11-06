e3600e78f4882741aea49848d8a007b032dfadb7
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-11-06 18:13:02 +0200

    Commit 2022-11-06 18:13

--- training_analysis/GC_read_activities.R
+++ training_analysis/GC_read_activities.R
@@ -169,5 +169,3 @@ if (length(files)!=0) {
 
 
 ####_ END _####
-tac = Sys.time()
-cat(sprintf("\n%s H:%s U:%s S:%s T:%f mins\n\n",Sys.time(),Sys.info()["nodename"],Sys.info()["login"],Script.Name,difftime(tac,tic,units="mins")))
