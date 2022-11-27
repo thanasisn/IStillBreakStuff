c7fd7d489e874103c3058ffa97f98d18c02411b5
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-11-27 12:13:01 +0200

    Commit 2022-11-27 12:13

--- training_analysis/GAR_read_download.R
+++ training_analysis/GAR_read_download.R
@@ -16,16 +16,43 @@ datalocation <- "~/ZHOST/ggg/504717a5-34a4-46c7-aa2c-3e34d7581984_1/DI_CONNECT/"
 
 
 library(data.table)
-
+library(jsonlite)
 
 
 
 allfiles <- list.files(datalocation, recursive = TRUE, full.names = TRUE)
 
+allfiles <- allfiles[file.size(allfiles) > 10]
+
+
+
 ## file types
 table(sub( ".*\\.", "" , basename(allfiles)))
 
 
+####  parse json  ####
+jsonfls <- grep(".json$", allfiles, ignore.case = TRUE, value = TRUE)
+
+jsonfls[file.size(jsonfls) <= 300]
+
+
+fromJSON(grep("user_profile", jsonfls, value = T), flatten = F)
+jsonfls <- grep("user_profile", jsonfls, value = T, invert = T)
+
+groups <- unique(sub( "__", "", sub(".json","", gsub( "[-[:digit:]]+", "", basename( jsonfls )))))
+
+
+for (ag in groups) {
+    pfiles <- agrep(ag, jsonfls, value = T)
+
+    cat("\n\nGroup:", ag, length(pfiles),"\n")
+    cat(pfiles,"\n")
+
+
+
+
+
+}
 
 
 
