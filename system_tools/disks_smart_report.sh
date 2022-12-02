a0fbcddfde5d056702c4ec1543ed850972dc0b52
thanasisn <11601717+thanasisn@users.noreply.github.com>
2022-12-02 15:07:03 +0200

    Commit 2022-12-02 15:07

--- system_tools/disks_smart_report.sh
+++ system_tools/disks_smart_report.sh
@@ -13,35 +13,36 @@ fi
 
 set +x
 
-## Variables
+## Variables init
 USER="athan"
 LOGDIR="$HOME/LOGs/SYSTEM_LOGS"
-
 mkdir -p "$LOGDIR"
 
-
 ## Loop all devices
-find "/dev/" -name "*sd?" | while read ad; do
+for ad in /dev/sd[a-z] /dev/sd[a-z][a-z]; do
+    if [[ ! -e $ad ]]; then continue ; fi
+    echo ""
     echo "Doing: $ad"
-
     ## get info we need
     data="$(sudo smartctl -a "$ad")"
-    ## prepare data
-    model="$(   echo "$data" | grep -i "device model:"    | sed 's/[ ]\+/ /g' | cut -d":" -f2- | sed 's/^[ ]*//' | sed 's/[ ]\+/-/')"
-    serial="$(  echo "$data" | grep -i "serial number:"   | sed 's/[ ]\+//g'  | cut -d":" -f2- | sed 's/^[ ]*//' | sed 's/[ ]\+/-/')"
-    hourson="$( echo "$data" | grep -i "Power_On_Hours"   | sed 's/[ ]\+/ /g' )"
-    minhoron="$(echo "$data" | grep -i "Power_On_Minutes" | sed 's/[ ]\+/ /g' )"
-
+    ## prepare info data
+    model="$(   echo "$data" | grep -i "device model:"    | sed 's/[ ]\+/ /g' | cut -d":" -f2- | sed 's/^[ ]*//g' | sed 's/[ ]\+/-/g')"
+    serial="$(  echo "$data" | grep -i "serial number:"   | sed 's/[ ]\+//g'  | cut -d":" -f2- | sed 's/^[ ]*//g' | sed 's/[ ]\+/-/g')"
+    hourson="$( echo "$data" | grep -i "Power_On_Hours"   | sed 's/[ ]\+/ /g' | grep -o "[0-9]\+[ ]*$")"
+    minhoron="$(echo "$data" | grep -i "Power_On_Minutes" | sed 's/[ ]\+/ /g' | cut -d" " -f11 | sed 's/h.*//g')"
+    ## ouput file
     outfile="${LOGDIR}/${model}_${serial}.smart"
-
+    ## generate report
     (
         echo ""
         date +"====  %F %R  ===="
         echo ""
         echo "Mounted on: $(hostname)"
         echo ""
-        echo "$hourson"
-        echo "$minhoron"
+        echo "Hours on:  $hourson"
+        echo "Days on:   $((hourson/24))"
+        echo "On:        $((hourson/24/365)) years  $((hourson/24 - 365*(hourson/24/365))) days"
+        echo "Minutes on: $minhoron"
         echo ""
         echo "** smartctl -H **"
         sudo smartctl -H "$ad"
@@ -53,10 +54,8 @@ find "/dev/" -name "*sd?" | while read ad; do
         sudo smartctl -x "$ad"
         echo ""
     ) | tee "$outfile"
-
     chmod a+rw  "$outfile"
     echo "$outfile"
-
 done
 
 exit 0
