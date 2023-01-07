895b44c7600db91c0eee12ad57bc30ee19be986a
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-01-07 19:07:02 +0200

    Commit 2023-01-07 19:07

--- system_tools/sub_messages2.py
+++ system_tools/sub_messages2.py
@@ -12,120 +12,87 @@ Copyright 2018 Athanasios Natsis
 
 ## read all messages from other systems and display them on local
 
-
-# Save as client.py
-# Message Sender
+import sys
+import pickle
+import zlib
+import base64
+import datetime
+import platform
 import os
 from socket import *
-import datetime
-import time
+from tendo import singleton
+# will sys.exit(-1) if other instance is running
+# lock = singleton.SingleInstance()
 
-host = "127.0.0.1" # set to IP address of target computer
-host = "10.12.12.1" # set to IP address of target computer
-port = 13000
-addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
-while True:
-    #data = raw_input("Enter message to send or type 'exit': ")
-    data = "Blue" + str(datetime.datetime.now())
-    UDPSock.sendto(data, addr)
-    if data == "exit":
-        break
-    time.sleep(.7)
-UDPSock.close()
-os._exit(0)
+from cryptography.fernet import Fernet
 
 
-import sys
-
-sys.exit()
-
+RANDOM  = "/home/athan/BASH/PARAMS/random.txt"
+HOST    = platform.node()
 
 
-import sys
-import os
-import pickle
-import zmq
-import time
-#from simplecrypt import encrypt, decrypt
-import base64
-import zlib
-import datetime
-import commands
-
-#### UI socket
-# Socket to hear from server
-PORT     = "45401"
-RANDOM   = "/home/athan/BASH/PARAMS/random.txt"
-
-## Set a password from a list of pass ##
-command = "tail -n+" + datetime.datetime.utcnow().strftime("%j") + " " + RANDOM + " | head -n1"
-res, PASSWORD = commands.getstatusoutput(command)
-
-print res, PASSWORD
-
+## Read list of passwords ##
 if ( os.path.isfile(RANDOM) ):
-    print "found list"
+    print("found list")
+    with open(RANDOM) as f:
+        PASSWORDS = f.readlines()
+    PASSWORDS = [x.strip() for x in PASSWORDS]
 else:
-    sys.exit("Could not find file " + RANDOM )
-
-if (res == 0) & (len(PASSWORD) > 10 ):
-    print "got a password"
-else:
-    sys.exit("Could not find proper password")
+    sys.exit("Could not find file" + RANDOM )
 
 
+def passwordf():
+    """
+    get correct password in each call
+    """
+    a_pass = PASSWORDS[int(datetime.datetime.utcnow().strftime("%j"))]
+    if len(a_pass) < 5:
+        sys.exit("Could not find proper password")
+    return(a_pass)
 
 
-def encode(key, clear):
-    enc = []
-    for i in range(len(clear)):
-        key_c = key[i % len(key)]
-        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
-        enc.append(enc_c)
-    return base64.urlsafe_b64encode("".join(enc))
+import secrets
 
-def decode(key, enc):
-    dec = []
-    enc = base64.urlsafe_b64decode(enc)
-    for i in range(len(enc)):
-        key_c = key[i % len(key)]
-        dec_c = chr((256 + ord(enc[i]) - ord(key_c)) % 256)
-        dec.append(dec_c)
-    return "".join(dec)
+from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
 
+from cryptography.fernet import Fernet
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
+backend = default_backend()
+iterations = 100_000
 
-host = "localhost"
-host = "155.207.9.18"
+def _derive_key(password: bytes, salt: bytes, iterations: int = iterations) -> bytes:
+    """Derive a secret key from a given password and salt"""
+    kdf = PBKDF2HMAC(
+        algorithm=hashes.SHA256(), length=32, salt=salt,
+        iterations=iterations, backend=backend)
+    return b64e(kdf.derive(password))
 
+def password_encrypt(message: bytes, password: str, iterations: int = iterations) -> bytes:
+    salt = secrets.token_bytes(16)
+    key = _derive_key(password.encode(), salt, iterations)
+    return b64e(
+        b'%b%b%b' % (
+            salt,
+            iterations.to_bytes(4, 'big'),
+            b64d(Fernet(key).encrypt(message)),
+        )
+    )
 
-context = zmq.Context()
-global socket
+def password_decrypt(token: bytes, password: str) -> bytes:
+    decoded = b64d(token)
+    salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
+    iterations = int.from_bytes(iter, 'big')
+    key = _derive_key(password.encode(), salt, iterations)
+    return Fernet(key).decrypt(token)
 
-socket = context.socket(zmq.SUB)
-socket.connect("tcp://%s:%s" % (host, PORT))
-socket.setsockopt(zmq.SUBSCRIBE, '')
+message = 'John Doe'
+password = 'mypass'
 
-
-
-while True:
-    packet = socket.recv()
-    #packet = zlib.decompress(packet)
-    #packet = decode(password, packet)
-
-    date, host, summary, body, urgency, expire_time = pickle.loads(packet)
-
-    print ""
-    print "Date           : ", date
-    print "From host      : ", host
-    print "Summary        : ", summary
-    print "Body           : ", body
-    print "Urgency        : ", urgency
-    print "Expiration time: ", expire_time
-
-    print datetime.datetime.now()
-    time.sleep(0.3)
-
-
+token = password_encrypt(message.encode(), password)
+print(token)
+mm = password_decrypt(token, password).decode()
+print(mm)
 
