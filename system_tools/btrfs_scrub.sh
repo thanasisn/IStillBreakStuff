1fb5426149f0e95dae24cd736a681ae3b0e458cb
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-01-15 13:07:03 +0200

    Commit 2023-01-15 13:07

--- system_tools/btrfs_scrub.sh
+++ system_tools/btrfs_scrub.sh
@@ -14,14 +14,26 @@ chmod a+rw  "$logfile"
 exec  > >(tee -i "${logfile}")
 exec 2> >(tee -i "${logfile}" >&2)
 
+echo "BTRFS partitions to scrub:"
+lsblk -f | grep  "btrfs" | grep -o " /.*"
 
 lsblk -f | grep  "btrfs" | grep -o " /.*" | while read device; do
     echo "** Scrub btrfs $device"
-    sudo /usr/bin/btrfs scrub start -B  "$device"
+    sudo /usr/bin/btrfs scrub start -B -d "$device"
+    echo "---------------------------------------------"
+    echo "** Status btrfs $device"
+    sudo /usr/bin/btrfs scrub status -d -R "$device"
     echo "---------------------------------------------"
 done
 
 echo
 echo "There is and a 'btrfs check' option, to use with care"
+
+
+
+
+
+
+
 
 exit 0
