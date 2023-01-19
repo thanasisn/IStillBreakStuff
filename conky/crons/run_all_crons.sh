a91f54fee2f86d441bc0ad01b3b48f88ab8601fc
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-01-19 09:07:04 +0200

    Commit 2023-01-19 09:07

--- conky/crons/run_all_crons.sh
+++ conky/crons/run_all_crons.sh
@@ -16,7 +16,11 @@ nice -n 19 ionice -c2 -n7 "${BASEPATH}cron_period_90.sh" &
 
 ## keep only last n lines of all status files
 find "/home/athan/LOGs/SYSTEM_LOGS/" -iname "*.status" | while read line; do
+    ## preserve time
+    mtime="$(stat -c %y "$line")"
+    ## need echo to pipe to the same file
     echo "$(tail -2000 $line)" > "$line"
+    touch -d "$mtime" "$line"
 done
 
 exit 0
--- system_tools/btrfs_scrub.sh
+++ system_tools/btrfs_scrub.sh
@@ -7,7 +7,7 @@ LOGDIR="/home/athan/LOGs/SYSTEM_LOGS"
 
 mkdir -p "$LOGDIR"
 
-logfile="${LOGDIR}/Btrfs_scrub_$(hostname)_$(date +'%F').check"
+logfile="${LOGDIR}/$0_$(hostname)_$(date +'%F').check"
 echo " " > "$logfile"
 chmod a+rw  "$logfile"
 
@@ -18,12 +18,13 @@ echo "BTRFS partitions to scrub:"
 lsblk -f | grep  "btrfs" | grep -o " /.*"
 
 lsblk -f | grep  "btrfs" | grep -o " /.*" | while read device; do
-    echo "** Scrub btrfs $device"
+    echo ""
+    echo " ** Scrub btrfs $device ** "
     sudo /usr/bin/btrfs scrub start -B -d "$device"
-    echo "---------------------------------------------"
+    echo "---------------------------------------------------"
     echo "** Status btrfs $device"
     sudo /usr/bin/btrfs scrub status -d -R "$device"
-    echo "---------------------------------------------"
+    echo "---------------------------------------------------"
 done
 
 echo
