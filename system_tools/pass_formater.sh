c42f4572b1073724790e8a11ac934f187e60b5af
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-02-05 21:07:03 +0200

    Commit 2023-02-05 21:07

---
+++ system_tools/pass_formater.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+## created on 2023-02-05
+
+#### Organize 'pass' entries with a uniform manner "./url/login"
+## Reads "url:" and "login:" entries and move to new location
+## Ignores entries with missing fields
+## Ignores identical locations
+## Move entries interactively
+## Overwrite protection is provided by 'pass mv' question
+
+## pass folder location
+prefix="$HOME/.password-store"
+
+while read line <&3; do
+    echo " --- "
+    entry="$(echo "$line" | sed 's@'"$prefix/"'@@')"
+    entry="${entry%.gpg}" 
+    # echo "$entry"
+    cont="$(pass "$entry")"
+    ## get base url
+    burl="$(echo "$cont" | grep "^url:" | sed -e 's|^[^/]*//||' -e 's|^www\.||' -e 's|/.*$||')"
+    ## get login
+    blog="$(echo "$cont" | grep "^login:" | sed 's|login:[ ]\+||')"
+    newentry="${burl}/${blog}"
+
+    [ -z "$burl" ] && echo "empty url $entry" && continue
+    [ -z "$blog" ] && echo "empty log $entry" && continue
+    [ "$newentry" == "$entry" ] && echo "SKIP SAME" && continue 
+
+    echo "OLD:  $entry"
+    echo "NEW:  ${burl}/${blog}"
+
+    echo ""
+    read -p "$entry ==> $newentry ? " conf
+    if   [[ $conf == "y" ]]; then
+        echo "move to new location"
+        pass mv "$entry" "$newentry" 
+    fi
+
+done 3< <(find "$prefix" -iname "*.gpg" | sort ) 
+
+
+exit 0 
