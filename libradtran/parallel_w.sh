6849be78d1f2fe3e6e76a4f20a8821e531463c97
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-02-14 10:13:01 +0200

    Commit 2023-02-14 10:13

--- libradtran/parallel_w.sh
+++ libradtran/parallel_w.sh
@@ -2,17 +2,14 @@
 
 #### Worker to run one instance of uvspec
 
-#### put uvspec execution inside these parenthesis
+#### put uvspec execution inside 
+
+## example code
 RR=$((RANDOM%10+1))
 sleep $RR
+
 printf " run: %3s   slept: %3s \n" $1 $RR;
 #### uvspec end
 
 exit
-#### put uvspec execution inside these parenthesis
-RR=$((RANDOM%20+1))
-sleep $RR
-printf " run: %3s   slept: %3s \n" $1 $RR;
-#### uvspec end
-
 
