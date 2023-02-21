ca347890bb2893fe6e92f7294435939b55a41a25
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-02-21 11:07:06 +0200

    Commit 2023-02-21 11:07

--- training_analysis/GC_test_metrics_outliers.R
+++ training_analysis/GC_test_metrics_outliers.R
@@ -60,7 +60,7 @@ metricsM <- rm.cols.dups.DT(metricsM)
 #+ results="asis", include=T, echo=F
 for (asp in unique(metricsM$Sport)) {
     cat(paste("\n\\newpage\n"))
-    cat(paste("\n\n# ",asp,"\n\n"))
+    cat(paste("\n\n# ", asp, "\n\n"))
 
     metrics  <- metricsM[ Sport == asp, ]
     metrics  <- rm.cols.dups.DT(metrics)
@@ -68,40 +68,43 @@ for (asp in unique(metricsM$Sport)) {
 
     if (nrow(metrics) <= 1) next()
 
-    wecare  <- names(metrics)[!sapply(metrics, is.character)]
-    wecare  <- grep("Date",      wecare, value = T, invert = T )
-    # wecare  <- grep("Time_in_Zone",      wecare, value = T, invert = T )
-    # wecare  <- grep("Time_in_Pace_Zone", wecare, value = T, invert = T )
-    # wecare  <- grep("date|notes|time|sport|workout_code|bike|shoes|workout_title|device|Calendar_text|Elevation_Gain_Carrying|heartbeats|Max_Core_Temperature|Checksum|Right_Balance|Percent_in_Zone|Percent_in_Pace_Zone|Best_|Distance_Swim|Equipment_Weight|Average_Core_Temperature|Average_Temp|Max_Cadence|Max_Temp|min_Peak_Pace|_Peak_Pace|_Peak_Pace_HR|_Peak_Power|_Peak_Power_HR|min_Peak_Hr|_Peak_WPK|Min_temp|Average_Cadence|Average_Running_Cadence|Max_Running_Cadence|Percent_in_Pace_Zone|moderate_fatigue|heavy_fatigue|low_fatigue|severe_fatigue|[0-9]_HRV",
-                    # wecare, ignore.case = T,value = T,invert = T)
-
+    wecare <- names(metrics)[!sapply(metrics, is.character)]
+    wecare <- grep("Date|Parsed_On", wecare, value = T, invert = T )
+    wecare <- grep("_V2$|_V3$",      wecare, value = T, invert = T )
+    wecare <- grep("Pch",            wecare, value = T, invert = T )
+    wecare <- grep("_Temp",          wecare, value = T, invert = T )
     wecare <- sort(wecare)
 
 
     for (var in wecare) {
         cat(paste("\n\\newpage\n"))
-        cat(paste("\n\n## Sport class:",var,"\n\n"))
+        cat(paste("\n\n## ", var, "\n\n"))
 
         temp    <- data.table(metrics$Date,  metrics[[var]])
         temp$V1 <- as.numeric(temp$V1)
         temp    <- temp[!is.na(V2)]
 
-        outlier_values <- boxplot.stats(temp$V2)$out  # outlier values.
+        ## ignore few data
+        if (nrow(temp) <= 2) next()
+        ## ignore low data variation
+        if (length(unique(temp$V2)) <= 4) next()
 
-        # boxplot(temp$V2, main=var, boxwex=0.1)
-        # mtext(paste("Outliers: ", paste(outlier_values, collapse=", ")), cex=0.6)
-        # cat(paste("\n\n"))
+        outlier_values <- boxplot.stats(temp$V2)$out  # outlier values.
+        outliers       <- temp[ V2 %in% outlier_values]
+
 
         # For continuous variable (convert to categorical if needed.)
-        boxplot(V2 ~ V1, data=temp, main=var)
+        boxplot(V2 ~ V1, data = temp, main = var)
         cat(paste("\n\n"))
+
         boxplot(V2 ~ cut(V1, pretty(temp$V1 )), data=temp, main=var, cex.axis=0.5)
         cat(paste("\n\n"))
 
-        temp$V1 <- as.Date(temp$V1, origin = "1970-01-01")
-        # temp$V1 <- as.POSIXct(temp$V1)
+        # temp$V1 <- as.Date(temp$V1, origin = "1970-01-01")
+        temp$V1 <- as.POSIXct(temp$V1, origin = "1970-01-01")
 
         plot(temp$V1, temp$V2)
+        points(outliers$V1, outliers$V2, col = "red")
         title(var)
         cat(paste("\n\n"))
 
@@ -111,12 +114,16 @@ for (asp in unique(metricsM$Sport)) {
         mod         <- lm( as.numeric(V1) ~ V2, data = temp)
         temp$cooksd <- cooks.distance(mod)
 
+        if (all(is.na(temp$cooksd))) next()
+
+        kklimit <- 4.5
+
         plot(temp$V1, temp$cooksd, pch = "*", cex = 2, main = var)  # plot cook's distance
-        abline(h = 4*mean(temp$cooksd, na.rm = T), col = "red")  # add cutoff line
+        abline(h = kklimit * mean(temp$cooksd, na.rm = T), col = "red")  # add cutoff line
         # text(x=1:length(cooksd)+1, y=cooksd, labels=ifelse(cooksd>4*mean(cooksd, na.rm=T),names(cooksd),""), col="red")  # add labels
         cat(paste("\n\n"))
 
-        pp <- temp[ cooksd > 4 * mean(cooksd, na.rm=T), V2, V1 ]
+        pp <- temp[ cooksd > kklimit * mean(cooksd, na.rm=T), V2, V1 ]
         setorder(pp, V2)
         names(pp) <- c("Date",var)
         cat(pander::pander( pp ))
