fe17facd1e24aa4f28fcac3b2cde5fa14ac13c9d
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-03-27 12:13:01 +0300

    Commit 2023-03-27 12:13

---
+++ data_tools/Parquet_dump.R
@@ -0,0 +1,56 @@
+#!/usr/bin/env Rscript
+
+####  Dump *.Rds files to a csv like format
+
+Sys.setenv(TZ = "UTC")
+library(arrow, warn.conflicts = FALSE, quietly = TRUE)
+
+args <- commandArgs(TRUE)
+
+suppressPackageStartupMessages(
+    USE_GDATA <- require(gdata, quietly = TRUE, warn.conflicts = FALSE)
+)
+
+for (fl in args) {
+    if (!file.exists(fl)) {
+        warning(paste("NOT A FILE:",fl))
+    } else {
+        ## read file
+        tmp <- try(read_parquet(fl))
+        cat("\r\n")
+        cat("## file:",fl,"\r\n")
+        cat("\r\n")
+        if (USE_GDATA) {
+            suppressPackageStartupMessages(library(gdata))
+            ## use a nicer formatter
+            write.fwf(x = tmp,
+                      append   = FALSE,
+                      quote    = FALSE,
+                      sep      = " ; ",
+                      eol      = "\r\n",  ## for unfortunate people with windows
+                      na       = "NA",
+                      rownames = FALSE,
+                      colnames = TRUE,
+                      qmethod  = c("escape", "double")
+            ## TODO maybe this don't needs gdata
+            ## Pipe it to   ... | column -t -s ";" -o " "
+            ## Pipe it to   ... | sed '/#.*/d' | column -t -s ";" -o " "
+            )
+        } else {
+            ## output to terminal with native tool
+            write.table(x            = format(tmp),
+                        append       = FALSE,
+                        quote        = FALSE,
+                        sep          = " ; ",
+                        eol          = "\r\n",  ## for unfortunate people with windows
+                        na           = "NA",
+                        dec          = ".",
+                        row.names    = FALSE,
+                        col.names    = TRUE,
+                        qmethod      = c("escape", "double"),
+                        fileEncoding = ""
+            )
+        }
+        cat("\r\n")
+    }
+}
---
+++ data_tools/Parquet_info.R
@@ -0,0 +1,29 @@
+#!/usr/bin/env Rscript
+
+####  Display info for *.parquet files
+
+Sys.setenv(TZ = "UTC")
+library(arrow, warn.conflicts = FALSE, quietly = TRUE)
+
+args <- commandArgs(TRUE)
+
+for (fl in args) {
+    if (!file.exists(fl)) {
+        warning(paste("NOT A FILE:",fl))
+    } else {
+        cat(paste0(rep("=",nchar(fl)), collapse = ""), "\n")
+        cat(paste(fl), "\n")
+        tmp <- try(read_parquet(fl))
+        cat(paste0(rep("-", nchar(fl)), collapse = ""), "\n")
+        cat(paste("Name   :", basename(fl)), "\n")
+        cat(paste("Rows   :", nrow(tmp)),    "\n")
+        cat(paste("Columns:", ncol(tmp)),    "\n")
+        # cat(paste("[", 1:length(names(tmp)), "]", names(tmp)), sep = "\n")
+        cat(paste0(" - str ", paste0(rep("-", nchar(fl) - 7), collapse = "")), "\n")
+        cat(str(tmp, nchar.max = 200))
+        cat("\n")
+        cat(paste0(" - summary ",paste0(rep("-", nchar(fl) - 11), collapse = "")), "\n")
+        print(summary(tmp), width = 120, na.print = "-NA-")
+        cat("\n")
+    }
+}
