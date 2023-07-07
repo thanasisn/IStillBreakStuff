8873e100550379b6cafd7d83b3067b3f65fbec1c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2023-07-07 14:13:01 +0300

    Commit 2023-07-07 14:13

--- passmark_py/cpu_scrap.py
+++ passmark_py/cpu_scrap.py
@@ -8,12 +8,11 @@ Using: https://github.com/ading2210/passmark-scraper
 @author: athan
 """
 
-
 from scraper import Scraper
-import pandas as pd
+import pandas   as pd
 import tabulate
 
-
+## get data fro CPUs
 scraper = Scraper("www.cpubenchmark.net")
 
 ## search for a specific term
@@ -32,9 +31,9 @@ res.append(scraper.search(query="i7-8700",          limit=1)[0][0])
 res.append(scraper.search(query="i5-6440HQ",        limit=1)[0][0])
 res.append(scraper.search(query="i5-7200U",         limit=1)[0][0])
 res.append(scraper.search(query="i7-5600U",         limit=1)[0][0])
-res.append(scraper.search(query="Xeon Silver 4108", limit=1)[0][0]) # yperos
+res.append(scraper.search(query="Xeon Silver 4108", limit=1)[0][0])  # yperos
 
-
+## convert to pandas
 data = pd.DataFrame.from_dict(res)
 
 # cnumeric = ['cpumark']
@@ -43,37 +42,29 @@ data = pd.DataFrame.from_dict(res)
 # data[cnumeric] = data[cnumeric].astype(float)
 # data.iloc[:,:].str.replace(',', '').astype(float)
 
-## conveert tu numeric
+## convert to numeric
 data.cpumark = data.cpumark.str.replace(',','').astype(float)
 data.thread  = data.thread .str.replace(',','').astype(float)
 
 ## set known machines fro id
 ## is id stable??
-data.loc[data["id"]=="1973", "id"] = 'tyler'
-data.loc[data["id"]=="2361", "id"] = 'blue'
-data.loc[data["id"]=="3099", "id"] = 'sagan'
-data.loc[data["id"]=="3167", "id"] = 'yperos'
-data.loc[data["id"]=="763",  "id"] = 'crane'
+data.loc[data["id"] == "1973", "id"] = 'tyler'
+data.loc[data["id"] == "2361", "id"] = 'blue'
+data.loc[data["id"] == "3099", "id"] = 'sagan'
+data.loc[data["id"] == "3167", "id"] = 'yperos'
+data.loc[data["id"] == "763",  "id"] = 'crane'
 
-## use tyler as vase
+## use tyler as base
 refmark   = data.cpumark[data.id == "tyler"]
 refthread = data.thread[data.id  == "tyler"]
 data['Rel cpumark'] = data.cpumark /   refmark.squeeze()
 data['Rel thread']  = data.thread  / refthread.squeeze()
 
-
+## clean duplicates and save all data
 data = data.drop_duplicates()
 data.to_csv('CPU_scrap_data.csv')
 
-## print pandas dt full table
-# print(tabulate.tabulate(data, headers='keys', tablefmt='rst', showindex="no"))
-
-
-
-# ## print a nice format table in console without pandas
-
-# ## remove some keys from dictionaries
-# cres = []
+## ignore these keys
 removekeys = ['price',
               'secondaryCores',
               'secondaryLogicals',
@@ -86,31 +77,37 @@ removekeys = ['price',
               'cpuCount',
               'threadValue',
               'cat']
+
+# ## print a nice format table in console without pandas
+# ## remove some keys from dictionaries
+# cres = []
 # for ar in res:
 #     for k in removekeys:
 #         ar.pop(k, None)
 #         # print(ar)
 #     cres.append(ar)
-
+#
 # ## create a nice table
 # header = cres[0].keys()
 # rows = [x.values() for x in cres]
 # print(tabulate.tabulate(rows, header))
 
-
-
-## print with pandaw
+## print a nice pandas table to terninal
 pp = data.drop(removekeys, axis=1)
 pp = pp.round(3)
 pp = pp.sort_values(by=['cpumark','thread'], ascending=False)
 
+## rearrange columns
 pp.insert(1, 'Rel cpumark', pp.pop('Rel cpumark'))
 pp.insert(2, 'Rel thread',  pp.pop('Rel thread'))
 pp.insert(3, 'cpumark',     pp.pop('cpumark'))
 pp.insert(4, 'thread',      pp.pop('thread'))
 
-
+## output
+print("")
 print(tabulate.tabulate(pp,
                         headers='keys',
                         tablefmt='simple',
-                        showindex="no"))
+                        showindex="no")
+      )
+print("")
