c6623e2cfe5911913879f135bac8c1f85c49395e
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-01-11 09:08:31 +0200

    Commit 2024-01-11 09:08

--- system_tools/auto_make.sh
+++ system_tools/auto_make.sh
@@ -2,6 +2,7 @@
 ## created on 2022-12-18
 
 #### Just a cron job to run all Makefiles 
+## Use it mainly for document production
 
 # allow only one instance
 exec 9>"/dev/shm/$(basename $0).lock"
@@ -9,7 +10,6 @@ if ! flock -n 9  ; then
     echo "another instance is running";
     exit 1
 fi
-
 
 info() { echo "$(date +%F_%T) ${SECONDS}s :: $* ::" >&1; }
 LOG_FILE="/dev/shm/$(basename "$0")_$(date +%F).log"
@@ -21,36 +21,29 @@ trap 'echo $( date +%F_%T ) ${SECONDS}s :: $0 interrupted ::  >&2;' INT TERM
 info "START :: $0 :: $* ::"
 
 
-if [[ "$(hostname)" = "tyler" ]]; then
-    echo "$(basename "$0") is suspended for now!!"
-    exit
-fi
-
-
-    #"$HOME/CS_id"
-    #"$HOME/MANUSCRIPTS/2022_sdr_trends"
+# if [[ "$(hostname)" = "tyler" ]]; then
+#     echo "$(basename "$0") is suspended in tyler!!"
+#     exit
+# fi
 
 ## list folders containing a makefile
 folders=(
     "$HOME/BBand_LAP/JOURNAL"
-    "$HOME/MISC/Redmi7_internal/documents"
     "$HOME/PANDOC/Journal"
-    "$HOME/PANDOC/Journal_Work"
     "$HOME/PANDOC/Notes"
+    "$HOME/PANDOC/Notes/09_JOURNAL_WORK"
     "$HOME/PANDOC/Notes_Aerosols"
-    "$HOME/PANDOC/Thesis"
 )
 
 for af in "${folders[@]}"; do
     info "$af"
     if [ -d "$af" ]; then
-        : 
-        # echo "$af exist"
+        info "Doing: $af"
     else
         info "$af don't exist SKIP!!"
         continue
     fi  
-    cd "$af"
+    cd "$af" || continue
     ## run make with default
     nice -n 19 ionice -c2 -n7 make -f *[Mm]akefile -C "$af"
     echo "================================="
