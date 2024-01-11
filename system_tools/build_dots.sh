cb53b6322fdc4f3bcae6009c53739a117cd05fa2
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-01-11 17:07:41 +0200

    Commit 2024-01-11 17:07

---
+++ system_tools/build_dots.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+## created on 2024-01-11
+
+#### Builds pdf and png for *.dot files in the folder
+## Use to update all dot graphs in a folder structure
+
+folder="$1"
+
+## check input
+if [ -d "$folder" ]; then
+    echo "Rucurse in: $folder" 
+    echo
+else
+    echo "Not a folder: $folder"
+    exit
+fi
+
+## run for all dot files
+find "$folder" -iname "*.dot" 2> /dev/null | while read line; do
+    dotfile="$line"
+    pdffile="${line%.[Dd][Oo][Tt]}.pdf"
+    pngfile="${line%.[Dd][Oo][Tt]}.png"
+    echo "$dotfile" 
+
+    if [ "$dotfile" -nt "$pdffile" ]; then
+        echo "Create PDF: $pdffile"
+        dot -Tpdf "$dotfile" -o "$pdffile"
+    fi
+
+    if [ "$dotfile" -nt "$pngfile" ]; then
+        echo "Create PNG: $pngfile"
+        dot -Tpng "$dotfile" -o "$pngfile"
+    fi
+done
+
+exit 0 
