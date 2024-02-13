329671b7aca691621bbb5072098b8351bb4c2301
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-02-13 18:13:08 +0200

    Commit 2024-02-13 18:13

--- system_tools/zfs_scrub.sh
+++ system_tools/zfs_scrub.sh
@@ -3,7 +3,7 @@
 #### Start a zfs scrub to check data integrity
 
 
-LOGDIR="/home/athan/LOGs/SYSTEM_LOGS"
+LOGDIR="/home/athan/LOGs/SYSTEM_LOGS/STORAGE"
 
 mkdir -p "$LOGDIR"
 
@@ -15,7 +15,7 @@ exec  > >(tee -i "${logfile}")
 exec 2> >(tee -i "${logfile}" >&2)
 
 echo "ZFS partitions to scrub:"
-zpool list -H -o name 
+zpool list -H -o name
 
 zpool list -H -o name | while read device; do
     echo "** Scrub zfs $device"
@@ -25,7 +25,6 @@ zpool list -H -o name | while read device; do
     sudo zpool status -v "$device"
     echo "---------------------------------------------"
 done
-
 
 
 exit 0
