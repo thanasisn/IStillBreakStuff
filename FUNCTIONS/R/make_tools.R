9aab4a721a7bfe46c9fb8fcc5cf151a715b4a773
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-03-09 11:07:17 +0200

    Commit 2024-03-09 11:07

--- FUNCTIONS/R/make_tools.R
+++ FUNCTIONS/R/make_tools.R
@@ -21,7 +21,7 @@
 #'
 #' @return            A string
 #'
-detext_source <- function(file,
+Rmk_detext_source <- function(file,
                            rm.comment = TRUE,
                            rm.space   = TRUE,
                            comment.ch = "#") {
@@ -43,12 +43,12 @@ detext_source <- function(file,
 
 #' Helper function to id a "source" file by hash
 #'
-#' @param file    A source file to id with `detext_source`
-#' @param ...     Parameters passed to `detext_source`
+#' @param file    A source file to id with `Rmk_detext_source`
+#' @param ...     Parameters passed to `Rmk_detext_source`
 #'
 #' @return        Data frame
 #'
-id_source <- function(file, ...) {
+Rmk_id_source <- function(file, ...) {
 
   require(digest, quietly = TRUE)
 
@@ -58,7 +58,7 @@ id_source <- function(file, ...) {
                path  = file,
              exist = file.exists(file),
                type  = "source",
-               hash  = digest(detext_source(file, ...), algo = "sha1", serialize = TRUE))
+               hash  = digest(Rmk_detext_source(file, ...), algo = "sha1", serialize = TRUE))
   } else {
     data.frame(mtime = NA,
                atime = NA,
@@ -76,7 +76,7 @@ id_source <- function(file, ...) {
 #'
 #' @return       A data frame
 #'
-id_data <- function(file) {
+Rmk_id_data <- function(file) {
 
   require(digest, quietly = TRUE)
 
@@ -110,30 +110,38 @@ id_data <- function(file) {
 
 
 
-read_Rmake <- function(file = "_Rmake.mc",
-                       path = "./") {
-  Rmkfile <- paste0(path, "/", file)
+
+parse_files <- function(depend.source = c(),
+                        depend.data   = c(),
+                        targets       = c()) {
+
+  ## parse source files
+  ss <- data.frame()
+  for (sf in depend.source) {
+    ss <- rbind(ss, Rmk_id_source(sf))
+  }
+  ## parse data files
+  dd <- data.frame()
+  for (df in depend.data) {
+    dd <- rbind(dd, Rmk_id_data(df))
+  }
+  ## parse target files
+  tt <- data.frame()
+  for (tf in targets) {
+    tt <- rbind(tt, Rmk_id_data(tf))
+    tt$type <- "target"
+  }
+
+  rbind(dd, ss, tt)
 }
 
 
-write_Rmake <- function(file = "_Rmake.mc",
-                        path = "./") {
-  Rmkfile <- paste0(path, "/", file)
-}
-
-read_Rmake()
-
-
-
-id_source("function.R", rm.commend = F)
-id_source("dfdsfs")
-id_data("./_targets/objects/drop_zeros")
 
 
 
 snap_Rmake <- function(depend.source = c(),
                        depend.data   = c(),
-                       file = "_Rmake.mc",
+                       file = ".R_make.mk",
                        path = "./") {
   Rmkfile <- paste0(path, "/", file)
 }
@@ -142,48 +150,63 @@ snap_Rmake <- function(depend.source = c(),
 ## Environment to pass make argumets
 R_make_ <- new.env(parent = emptyenv())
 
+
+
+
 check_Rmake <- function(depend.source = c(),
                         depend.data   = c(),
                         targets       = c(),
-                        file = "_Rmake.mc",
+                        file = ".R_make.mk",
                         path = "./") {
   ## default file to read depend
   Rmkfile <- paste0(path, "/", file)
 
-  ## srore some variables
+  ## store some variables
   R_make_$file          <- Rmkfile
   R_make_$depend.source <- depend.source
   R_make_$depend.data   <- depend.data
   R_make_$targets       <- targets
   R_make_$RUN           <- FALSE
 
-  ## parse source files
-  ss <- data.frame()
-  for (sf in depend.source) {
-    ss <- rbind(ss, id_source(sf))
+  ## parse dependencies and targets
+  new <- parse_files(
+    depend.source = depend.source,
+    depend.data   = depend.data,
+    targets       = targets
+  )
+
+  ## Inform and ignore missing dependencies
+  mis_dep <- new$type %in% c("source", "data") & new$exist == FALSE
+  if (any(mis_dep)) {
+    cat("Ignoring missing dependencies:\n")
+    cat(paste(" > ", new[mis_dep, "path"]), sep = "\n")
+    warning("Ignoring missing dependencies: ", new[mis_dep, "path"])
+    new <- new[!mis_dep, ]
   }
-  ## parse data files
-  dd <- data.frame()
-  for (df in depend.data) {
-    dd <- rbind(dd, id_data(df))
+
+  ## Check if we missing targets
+  mis_tar <- new$type  == "target" & new$exist == FALSE
+  if (any(mis_tar)) {
+    cat("Not existing targets:\n")
+    cat(paste(" < ", new[mis_tar, "path"]), sep = "\n")
+    cat("\nHAVE TO RUN\n\n")
+    R_make_$RUN <- TRUE
+    ## return
   }
-  ## parse target files
-  tt <- data.frame()
-  for (tf in targets) {
-    dd <- rbind(dd, id_source(tf))
-    dd$type <- "target"
+
+  ## Check if we know of previous runs
+  if (!file.exists(Rmkfile)) {
+    cat("No previous watch file found: ", Rmkfile, "\n")
+    cat("\nHAVE TO RUN\n\n")
+    R_make_$RUN <- TRUE
+  } else {
+    old <- read.csv(Rmkfile)
   }
 
-  new <- rbind(dd, ss, tt)
 
-  write.csv(new, Rmkfile, row.names = FALSE)
+  print(R_make_$RUN)
 
-
-
-
-
-
-  return(new)
+  return(list(new = new, old = old))
 
 
   ## Source hash changed from previous run
@@ -197,17 +220,43 @@ check_Rmake <- function(depend.source = c(),
 }
 
 
-new <- check_Rmake(depend.source = c( "function.R", "~/CODE/FUNCTIONS/R/make_tools.R"),
+out <- check_Rmake(depend.source = c("~/CODE/FUNCTIONS/R/make_tools.R"),
                    depend.data  = c("~/DATA/Broad_Band/Broad_Band_DB_metadata.parquet"),
-                   targets      = c("~/ZHOST/testfile") )
+                   targets      = c("~/ZHOST/testfile", "~/ZHOST/testfile2") )
+out
 
 
-new$type == "target"
+out <- check_Rmake(depend.source = c("~/CODE/FUNCTIONS/R/make_tools.R"),
+                   depend.data  = c("~/DATA/Broad_Band/Broad_Band_DB_metadata.parquet")
+                   )
+(out)
+
+new <- out$new
+old <- out$old
+
+## check for changes in source
+
+
+
+new_s <- new[new$type == "source", ]
+
+old_s <- old[old$type == "source", ]
+
+
+read.csv(R_make_$file)
+
 
 store_Rmake <- function() {
-  return(R_make_$depend.data)
+  write.csv(new, R_make_$file, row.names = FALSE)
 }
-
 store_Rmake()
 
+print(R_make_$file)
+
+
 write.csv(1, "~/ZHOST/testfile")
+
+
+Rmk_id_source("function.R", rm.commend = F)
+Rmk_id_source("dfdsfs")
+Rmk_id_data("./_targets/objects/drop_zeros")
