8aeebec6bf33bb7e3ad10e8115f0c8b9d40b7a5e
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-03-14 16:13:09 +0200

    Commit 2024-03-14 16:13

--- system_tools/new_bash.sh
+++ system_tools/new_bash.sh
@@ -1,34 +1,37 @@
 #!/usr/bin/env bash
 ## https://github.com/thanasisn <natsisphysicist@gmail.com>
 
+#### Create a new executable bash script in vim
 
-#### Create a new executable bash script
-
-## catch input and make valid name
-newscript="$1"
+## get input and make valid name
+newscript="${1:-test_script}"
 scriptname="${newscript%.sh}.sh"
+goto="8"
 
+# check file exist and create new name 
 if [[ -f "$scriptname" ]] ; then
-   echo "file exist choose new name"
-   exit 0
-else
-   echo "Will make:   $scriptname"
-
-   ## bash template
-   (printf "#!/usr/bin/env bash\n"
-   printf "## created on $(date +%F)\n\n"
-   printf "#### enter description here\n\n"
-   printf 'echo "i am $0 who the hell are you"\n'
-   printf 'TIC=$(date +"%%s")\n'
-   printf "## start coding\n\n\n\n\n"
-   printf "sleep 1\n\n\n\n\n"
-   printf "## end coding\n"
-   printf 'TAC=$(date +"%%s"); dura="$( echo "scale=6; ($TAC-$TIC)/60" | bc)"\n'
-   printf 'printf "%%s %%-10s %%-10s %%-50s %%f\\n" "$(date +"%%F %%H:%%M:%%S")" "$HOSTNAME" "$USER" "$(basename $0)" "$dura"\n'
-   printf "exit 0 \n") > "$scriptname"
-
-   ## make executable
-   chmod +x "$scriptname"
+  echo "File existsi, choosisng a new name..."
+  scriptname="${newscript%.sh}_$(date +'%F_%T').sh"
 fi
+
+echo "File: $scriptname"
+
+## bash template
+(printf "#!/usr/bin/env bash\n"
+printf "## created on %s\n\n" "$(date +%F)"
+printf "#### ..enter description here..\n\n"
+printf "## start coding\n"
+printf 'TIC=$(date +"%%s")\n\n\n'
+printf "sleep 1\n\n\n\n\n"
+printf "##  END  ##\n"
+printf 'TAC=$(date +"%%s"); dura="$( echo "scale=6; ($TAC-$TIC)/60" | bc)"\n'
+printf 'printf "%%s %%-10s %%-10s %%-50s %%f\\n" "$(date +"%%F %%H:%%M:%%S")" "$HOSTNAME" "$USER" "$(basename $0)" "$dura"\n'
+printf "exit 0 \n") > "$scriptname"
+
+## make executable
+chmod +x "$scriptname"
+
+## open for edit
+vim -c "$goto" "$scriptname"
 
 exit 0
--- system_tools/new_journal.sh
+++ system_tools/new_journal.sh
@@ -2,20 +2,21 @@
 ## created on 2022-12-19
 ## https://github.com/thanasisn <natsisphysicist@gmail.com>
 
-
-#### Just create a new .md file with a given local time or current date
+#### Create a new .md file with a given local time or current date and open for edit
 ## This is meant to run only within the Journal folder
+## Will create a folder structure for the file
 
-## parse input or create the date variable
+## Parse input or create the date variable  ------------------------------------
 args="$*"
 if [[ -z "$args" ]]; then
-    echo "Using current date"
-    stamp="$(date +"%s")"
+  echo "Using current date"
+  stamp="$(date +"%s")"
 else
-    stamp="$(date -d "${args}" +"%s")"
-    [[ $? -gt 0 ]] && echo "Can not parse input as date" && exit 1
+  stamp="$(date -d "${args}" +"%s")"
+  [[ $? -gt 0 ]] && echo "Can not parse input as date" && exit 1
 fi
 
+## Crete different variables for the template  ---------------------------------
 datestr="$(date    -d@"${stamp}" +"%F %H:%M %Z")"
 datestrs="$(date   -d@"${stamp}" +"%F %T %Z")"
 dateUTC="$(date -u -d@"${stamp}" +"%F %T %Z")"
@@ -29,28 +30,28 @@ filename="./${year}/${datenme}.md"
 
 ## open existing file if exist
 if [[ -f "$filename" ]] ; then
-    echo "File $filename exist"
-    vim "$filename"
-    echo "exit"
-    exit 0
+  echo "File $filename exist"
+  vim -c "$goto" "$filename"
+  echo "exit"
+  exit 0
 fi
 
 ## create new file with obsidian template
 echo  "Creating: $filename"
 touch "$filename"
 (
-    echo "---"
-    echo "tags:      [  ]"
-    echo "scope:     "
-    echo "created:   $datestrs"
-    echo "cunixtime: ${stamp}"
-    echo "UTC:       $dateUTC"
-    echo "---"
-    echo ""
-    echo "## ${datestr}"
-    echo ""
-    echo ""
-    echo ""
+  echo "---"
+  echo "tags:      [  ]"
+  echo "scope:     "
+  echo "created:   $datestrs"
+  echo "cunixtime: ${stamp}"
+  echo "UTC:       $dateUTC"
+  echo "---"
+  echo ""
+  echo "## ${datestr}"
+  echo ""
+  echo ""
+  echo ""
 ) > "$filename"
 
 ## open for edit
