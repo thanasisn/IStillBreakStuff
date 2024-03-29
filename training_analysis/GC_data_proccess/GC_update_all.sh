6b3fc86b182a0ecbdfa44c68828c7b4601fa3fc9
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-03-29 11:07:27 +0200

    Commit 2024-03-29 11:07

--- training_analysis/GC_data_proccess/GC_update_all.sh
+++ training_analysis/GC_data_proccess/GC_update_all.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 ## created on 2024-03-11
 
-#### enter description here
+#### Run all regular data proccess for GoldenCheetah
 
 ## Init
 mkdir -p "/dev/shm/CONKY"
 set +e
 pids=()
 
-## Parse data from GoldenGheetah data base
+## Parse data and plot data from GoldenGheetah
 "$HOME/CODE/training_analysis/GC_data_proccess/GC_read_rides_db_json.R"
 "$HOME/CODE/training_analysis/GC_data_proccess/GC_more_plots_rides_db.R"
 "$HOME/CODE/training_analysis/GC_data_proccess/GC_conky_plots_rides_db.R"
 
-## Create current plots
+# todo 
+"/home/athan/CODE/training_analysis/GC_data_proccess/GC_shoes_usage_duration.R"
 
 sleep 1 & pids+=($!)
 
