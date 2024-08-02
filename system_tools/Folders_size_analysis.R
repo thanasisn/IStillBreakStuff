1efc132b0036914f2168936fd6b7e9cbc6b1cca4
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-08-02 18:13:07 +0300

    Commit 2024-08-02 18:13

--- system_tools/Folders_size_analysis.R
+++ system_tools/Folders_size_analysis.R
@@ -64,9 +64,9 @@ datafls <- sort(datafls)
 SIZE_LIM <- 300 * 1024 ^ 2
 
 af <- datafls[1] # blue
-af <- datafls[4] # yperos
-af <- datafls[2] # sagan
 af <- datafls[3] # tyler
+af <- datafls[2] # sagan
+af <- datafls[4] # yperos
 
 # for (af in datafls) {
 host <- sub("Log_folders_size_", "", sub(".Rds", "", basename(af)))
