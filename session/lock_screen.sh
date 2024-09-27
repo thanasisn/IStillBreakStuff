e4624d78124112f5e6046394947d7e40397a62b2
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-09-27 17:07:28 +0300

    Commit 2024-09-27 17:07

--- session/lock_screen.sh
+++ session/lock_screen.sh
@@ -12,16 +12,26 @@ mkdir -p "/dev/shm/CONKY"
 resol="$(xrandr | grep -o " connected .*" | grep -o "[0-9]\+x[0-9]\+" | sort -n | tail -n 1)"
 echo "$resol"
 
-## create a proper image
-convert             "$BASE_IMG"    \
-        -gravity    "center"       \
-        -background "$COLOR"       \
-        -extent     "$resol"       \
-                    "$TARGET"
+## create a proper image for the current screen
+# convert             "$BASE_IMG"    \
+#         -gravity    "center"       \
+#         -background "$COLOR"       \
+#         -extent     "$resol"       \
+#                     "$TARGET"
 
-## lock screen and black screen
-( i3lock -t -c 000000 -i "$TARGET" ) &
-sleep 3
-( xset dpms force off ) &
+convert                                               \
+  -background     "$COLOR"                            \
+  -density 200    "$HOME/MISC/Media/IMAGES/nixos.svg" \
+  -gravity center "$BASE_IMG"                         \
+  -composite                                          \
+  -extent         "$resol"                            \
+  "$TARGET"
+
+## lock screen with tiled image
+nohup i3lock -t -c 000000 -i "$TARGET"
+sleep 15
+
+## blank screen
+xset dpms force off
 
 exit 0
