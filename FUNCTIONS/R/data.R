4a6efdb39c91b346996d2b81377fcd69b9a0f935
thanasisn <11601717+thanasisn@users.noreply.github.com>
2024-12-08 19:07:12 +0200

    Commit 2024-12-08 19:07

--- FUNCTIONS/R/data.R
+++ FUNCTIONS/R/data.R
@@ -219,3 +219,103 @@ fncols <- function(data, cname) {
     data
 }
 
+
+
+#' Finds consecutive instances of the same number.
+#'
+#' @param avector A vector to search for consecutive values
+#' @param limit Consecutive values threshold to be included in the output
+#'
+#' @return A list of vectors:
+#'     lengths: how many consecutive values were found
+#'     values:  the consecutive value found
+#'     starts:  the starting index of each of the consecutive set
+#'     ends:    the ending index of each of the consecutive set
+#' @export
+#' @family data manipulation functions
+#'
+find_freezed_measurements <- function(avector, limit) {
+    ## count repetitions
+    runs = rle(avector)
+    if (length( runs$lengths ) <= 0) print("Can not count repetitions")
+
+    ## find repetitions by limit
+    myruns = which( runs$lengths >= limit )
+    if (length(myruns) <= 0) print("No consecutive values found")
+
+    ## find indexes of ends of repetitions
+    runs.lengths.cumsum = cumsum(runs$lengths)
+    ends = runs.lengths.cumsum[myruns]
+
+    ## find indexes of start of repetitions
+    newindex = ifelse(myruns > 1, myruns - 1, 0)
+    starts = runs.lengths.cumsum[newindex] + 1
+    if (0 %in% newindex) starts = c(1, starts)
+
+    ## results
+    return(
+        list( lengths = runs$lengths,
+              values  = runs$values,
+              starts  = starts,
+              ends    = ends)
+    )
+}
+
+
+
+#' Find nearest numbers between vectors.
+#'
+#' @description Return an array `i` of indexes into `target`, parallel to array `probe`.
+#'              For each index `j` in `target`, `probe[i[j]]` is nearest to `target[j]`.
+#'              From: https://stats.stackexchange.com/questions/161379/quickly-finding-nearest-time-observation
+#'
+#' @param probe  A vector
+#' @param target A vector
+#'
+#' @return Indexes of `target` matching `probe` data.
+#' @export
+#' @family data manipulation functions
+#'
+#' @examples
+#' ## Graphical illustration.
+#' set.seed(17)
+#' x <- sort(round(runif(8), 3))
+#' y <- sort(round(runif(12), 1))
+#' i <- nearest(x, y)
+#' plot(c(0,1), c(3/4,9/4), type="n", bty="n", yaxt="n", xlab="Values", ylab="")
+#' abline(v = (y[-1] + y[-length(y)])/2, col="Gray", lty=3)
+#' invisible(apply(rbind(x, y[i]), 2, function(a) arrows(a[1], 1, a[2], 2, length=0.15)))
+#' points(x, rep(1, length(x)), pch=21, bg="Blue")
+#' points(y, rep(2, length(y)), pch=21, bg="Red", cex=sqrt(table(y)[as.character(y)]))
+#' text(c(1,1), c(1,2), c("x","y"), pos=4)
+#'
+nearest <- function(probe, target, ends=c(-Inf,Inf)) {
+    # Both `probe` and `target` must be vectors of numbers in ascending order.
+    if ( is.unsorted(probe ) ) { stop("Probe is not sorted") }
+    if ( is.unsorted(target) ) { stop("Target is not sorted") }
+
+    glb <- function(u, v) {
+        n <- length(v)
+        z <- c(v, u)
+        j <- i <- order(z)
+        j[j > n] <- -1
+        k <- cummax(j)
+        return(k[i > n])
+    }
+    y <- c(ends[1], target, ends[2])
+
+    i.lower <- glb(probe, y)
+    i.upper <- length(y) + 1 - rev(glb(rev(-probe), rev(-y)))
+    y.lower <- y[i.lower]
+    y.upper <- y[i.upper]
+    lower.nearest <- probe - y.lower < y.upper - probe
+    i <- ifelse(lower.nearest, i.lower, i.upper) - 1
+    i[i < 1 | i > length(target)] <- NA
+    return(i)
+}
+
+
+
+
+
+
