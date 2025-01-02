e89cb21aec9c6489858cf91208b4e88b952e1d32
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-01-02 22:13:07 +0200

    Commit 2025-01-02 22:13

--- R_MISC/my_publications/listofpublication.R
+++ R_MISC/my_publications/listofpublication.R
@@ -17,14 +17,12 @@
 #'  - \setmainfont[Scale=1]{Linux Libertine O}
 #'
 #' output:
-#'   html_document:
-#'     toc:        true
 #'   bookdown::pdf_document2:
 #'     keep_tex:         yes
 #'     keep_md:          yes
 #'     latex_engine:     xelatex
-#'     toc:              yes
-#'     toc_depth:        4
+#'   html_document:
+#'     keep_md:          yes
 #'
 #' date: "`r format(Sys.time(), '%F')`"
 #'
@@ -38,47 +36,99 @@ knitr::opts_chunk$set(messages = FALSE  )
 library(bib2df)
 library(stevemisc)
 library(stringi)
+library(janitor)
 
 bib_fl     <- "~/LIBRARY/A_Atmosphere/A_Atmosphere.bib"
 my_pattern <- "Natsis|Νάτσης"
+LANG       <- "en"
+LANG       <- "el"
 BOLD       <- TRUE
+# BOLD       <- FALSE
+NUMBER     <- TRUE
+# NUMBER     <- FALSE
 
-# load a bib file to data frame
+
+## load a bib file to data frame
 bib_df <- bib2df(file = bib_fl)
 bib_df$ABSTRACT <- NULL
 
-
 ## keep mine
 bib_df <- bib_df[grepl(my_pattern, bib_df$AUTHOR),]
-## order by recent
+bib_df <- remove_empty(bib_df, which = "cols")
+
+## order by date
 bib_df <- bib_df[order(bib_df$DATE, decreasing = T), ]
+
+## select categories
+unique(bib_df$CATEGORY)
+
+bib_df <- bib_df[
+  bib_df$CATEGORY %in% c(
+    "ARTICLE",
+    # "SOFTWARE",
+    "INPROCEEDINGS",
+    # "MISC",
+    # "MASTERSTHESIS",
+    # "THESIS",
+    NULL
+  ),
+]
+
+
 
 #+ results="asis"
 for (at in unique(bib_df$CATEGORY)) {
-  cat("\n\n##", at, "\n\n")
+  cc <- 0
+  ## Change language
+  if (LANG == "el") {
+    tit <- switch(
+      at,
+      ARTICLE       = "Άρθρα",
+      INPROCEEDINGS = "Παρουσιάσεις σε συνέδρια"
+    )
+  }
+  cat("\n\n##", tit, "\n\n")
 
   temp <- bib_df[bib_df$CATEGORY == at, ]
 
   for (i in 1:nrow(temp)) {
-
+    cc        <- cc + 1
     bib_entry <- paste0(capture.output(df2bib(temp[i,])), collapse = "")
 
     suppressMessages({
-      print_refs(bib_entry,
-                            csl = "apa.csl",
-                            spit_out = TRUE,
-                            delete_after = FALSE)
+      # print_refs(bib_entry,
+      #                       csl = "apa.csl",
+      #                       spit_out = TRUE,
+      #                       delete_after = FALSE)
 
-sub()
-      print_refs(bib_entry,
-                 csl = "apa.csl",
-                 spit_out = TRUE,
-                 delete_after = FALSE)
+      # paste(print_refs(bib_entry,
+      #            csl = "apa.csl",
+      #            spit_out = FALSE,
+      #            delete_after = FALSE))
 
+     if (NUMBER) {
+       cat(cc, ". ", sep = "")
+     }
 
-
+      if (BOLD) {
+        cat(
+          sub("(Natsis,[. AN]*)|(Νάτσης,[. ΑΝ]*)", "**\\1\\2**",
+              print_refs(bib_entry,
+                         csl = "apa.csl",
+                         spit_out = FALSE,
+                         delete_after = FALSE)
+          ), "\n"
+        )
+      } else {
+        cat(
+          print_refs(bib_entry,
+                     csl = "apa.csl",
+                     spit_out = FALSE,
+                     delete_after = FALSE)
+          , "\n"
+        )
+      }
     })
-stop()
     cat("\n")
   }
 }
