8b29204b114bd6d0a24431c108221289d3b361fe
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-01-14 16:13:09 +0200

    Commit 2025-01-14 16:13

--- R_MISC/rix_test/create_env.R
+++ R_MISC/rix_test/create_env.R
@@ -15,32 +15,37 @@ stop("Use only to create default.nix, we wan't to keep .Rprofile")
 
 library("rix")
 
+available_r()
+
 path_default_nix <- "."
 
 rix(
   r_ver        = "4.3.3",
+  # r_ver        = "latest-upstream",
   r_pkgs       = c(
-                   "data.table",
-                   "dplyr",
-                   "ggplot2",
-                   "renv",
-                  NULL),
-  git_pkgs     = list(
-                   list(package_name = "colorout",
-                        repo_url     = "https://github.com/jalvesaq/colorout/",
-                        commit       = "6eca95213c6cb2fae1c2c4eaccf43de4c93a65b5"),
-                   list(package_name = "duckdb",
-                        repo_url     = "https://github.com/duckdb/duckdb-r/",
-                        commit       = "71c71a6b824bd0f2c0fca43c5dfa53645b17bfb7")
-  ),
+    "data.table",
+    "dplyr",
+    "ggplot2",
+    "renv",
+    NULL),
+  # git_pkgs     = list(
+  #   list(package_name = "colorout",
+  #        repo_url     = "https://github.com/jalvesaq/colorout/",
+  #        commit       = "6eca95213c6cb2fae1c2c4eaccf43de4c93a65b5"),
+  #   list(package_name = "duckdb",
+  #        repo_url     = "https://github.com/duckdb/duckdb-r/",
+  #        commit       = "71c71a6b824bd0f2c0fca43c5dfa53645b17bfb7")
+  # ),
   system_pkgs  = c(
-                   "adwaita-qt",
-                   # "adwaita-qt6",
-                   NULL),
+    "adwaita-qt",
+    # "adwaita-qt6",
+    NULL),
   ide          = "rstudio",
   project_path = path_default_nix,
-  overwrite    = FALSE, ## don't use rix for adding packages
-  print        = TRUE
+  # overwrite    = FALSE, ## don't use rix for adding packages
+  print        = TRUE,
+  overwrite    = TRUE,
+  NULL
 )
 
 ## run nix build from r
