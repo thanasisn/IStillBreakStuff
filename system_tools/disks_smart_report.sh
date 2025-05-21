d829fdbbb7126200a0e299858370c09ca3b8e2d3
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-05-21 19:07:10 +0300

    Commit 2025-05-21 19:07

--- system_tools/disks_smart_report.sh
+++ system_tools/disks_smart_report.sh
@@ -26,7 +26,53 @@ cleanup() {
 
 trap cleanup 0 1 2 3 6
 
-## Loop all devices
+## Loop all nvme
+for ad in /dev/nvme[0-9]; do
+    if [[ ! -e $ad ]]; then continue ; fi
+    echo ""
+    echo "Doing: $ad"
+    ## get info we need
+    data="$(sudo smartctl -a "$ad")"
+    ## prepare info data
+
+    model="$(   echo "$data" | grep -i "model number:"    | sed 's/[ ]\+/ /g' | cut -d":" -f2- | sed 's/^[ ]*//g' | sed 's/[ ]\+/-/g')"
+    serial="$(  echo "$data" | grep -i "serial number:"   | sed 's/[ ]\+//g'  | cut -d":" -f2- | sed 's/^[ ]*//g' | sed 's/[ ]\+/-/g')"
+    hourson="$( echo "$data" | grep -i "Power On Hours"   | sed 's/[ ]\+/ /g' | grep -o "[0-9]\+[ ]*$")"
+    minhoron="$(echo "$data" | grep -i "Power_On_Minutes" | sed 's/[ ]\+/ /g' | cut -d" " -f11 | sed 's/h.*//g')"
+    ## ouput file
+    # outfile="${LOGDIR}/${model}_${serial}_$(date +%F).smart"
+    outfile="${LOGDIR}/${model}_${serial}.smart"
+    ## generate report
+    (
+        echo ""
+        date +"====  %F %R  ===="
+        echo ""
+        echo "Mounted on: $(hostname)"
+        echo ""
+        echo "Hours on:  $hourson"
+        echo "Days on:   $((hourson/24))"
+        echo "On:        $((hourson/24/365)) years  $((hourson/24 - 365*(hourson/24/365))) days"
+        echo "Minutes on: $minhoron"
+#        echo ""
+#        echo "** smartctl -H **"
+#        sudo smartctl -H "$ad"
+#        echo ""
+#        echo "** smartctl -a **"
+#        sudo smartctl -a "$ad"
+#        echo ""
+#        echo "** smartctl -x **"
+#        sudo smartctl -x "$ad"
+#        echo ""
+    ) | tee "$outfile"
+    chmod a+rw  "$outfile"
+    echo "$outfile"
+
+
+    echo "$data"
+done
+
+exit
+## Loop all hdd
 for ad in /dev/sd[a-z] /dev/sd[a-z][a-z]; do
     if [[ ! -e $ad ]]; then continue ; fi
     echo ""
