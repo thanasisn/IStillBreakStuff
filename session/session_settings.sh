21687611bee61ffd0e35633db41b89d523a07480
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-06-11 08:07:06 +0300

    Commit 2025-06-11 08:07

--- session/session_settings.sh
+++ session/session_settings.sh
@@ -4,7 +4,14 @@
 #### Set session options for monitors keyboard touchpad ...
 
 set +e
+
+## screen lock option
 case "$(hostname)" in
+    "mumra")
+        echo "mumra"
+        savertime=1100
+        ;;
+
     "tyler")
         echo "tyler"
         savertime=900
@@ -22,41 +29,42 @@ case "$(hostname)" in
 esac
 echo
 
-
 echo "-- screen saver and locking --"
 xset s $savertime $savertime
 echo xset s $savertime $savertime
 echo
 
-echo "-- set monitors standby suspend off --"
+echo "- - - Set monitors standby suspend off - - - "
 time=3000
 echo xset dpms $time $(( time + 120)) $((time + 160))
 xset dpms $time $(( time + 120)) $((time + 160))
 echo
 
-echo "-- keyboard repeat rate --"
+echo " - - - Keyboard repeat rate - - - "
 echo xset r rate 300 45
 xset r rate 300 45
 echo
 
-echo " - - - Current xset config - - -"
+echo " - - - Current xset config - - - "
 xset q
 echo
 
-echo "set touchpad options"
-synclient  VertEdgeScroll=1
-synclient  TapButton1=1
-synclient  TapButton2=1      # why on tyler
-synclient  LBCornerButton=1
-synclient  RBCornerButton=1
-echo
+if type synclient >/dev/null 2>&1; then
+  echo " - - - Set touchpad options - - - "
+  synclient  VertEdgeScroll=1
+  synclient  TapButton1=1
+  synclient  TapButton2=1      # why on tyler
+  synclient  LBCornerButton=1
+  synclient  RBCornerButton=1
+  echo
+fi
 
-echo "set keyboard language options"
+echo "Set keyboard language options"
 #  setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,el
 setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll us,gr
 echo
 
-echo "swap caps and escape keys"
+echo "Swap caps and escape keys"
 setxkbmap -option "caps:swapescape"
 echo
 
