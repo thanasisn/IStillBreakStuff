d362a0deb1084aeac20795d6b3b267377bb140a8
thanasisn <11601717+thanasisn@users.noreply.github.com>
2025-11-27 11:07:06 +0200

    Commit 2025-11-27 11:07

---
+++ R_MISC/nanopool_scrap.R
@@ -0,0 +1,172 @@
+#!/usr/bin/env Rscript
+# /* Copyright (C) 2023 Athanasios Natsis <natsisphysicist@gmail.com> */
+
+rm(list = (ls()[ls() != ""]))
+Script.Name <- "~/CODE/R_MISC/nanopool_scrap.R"
+Sys.setenv(TZ = "UTC")
+tic <- Sys.time()
+
+## __  Set environment ---------------------------------------------------------
+require(data.table, quietly = TRUE, warn.conflicts = FALSE)
+require(httr,       quietly = TRUE, warn.conflicts = FALSE)
+
+source("/home/athan/Formal/KEYS/nanopool")
+storagePre <- "~/DATA/Other/Nanopool_"
+
+
+# Miner - Account Balance
+# https://api.nanopool.org/v1/xmr/balance/:address
+#
+# Miner - Average Hashrate
+# https://api.nanopool.org/v1/xmr/avghashrate/:address
+#
+# Miner - Chart Data
+# https://api.nanopool.org/v1/xmr/hashratechart/:address
+#
+# Miner - Current Hashrate
+# https://api.nanopool.org/v1/xmr/hashrate/:address
+#
+# Miner - Hashrate History
+# https://api.nanopool.org/v1/xmr/history/:address
+#
+# Miner - List of Worker
+# https://api.nanopool.org/v1/xmr/workers/:address
+#
+# Miner - Share Rate History
+# https://api.nanopool.org/v1/xmr/shareratehistory/:address
+
+
+# tags <- c(
+#   # "balance",
+#   # "avghashrate",
+#   # "hashratechart",
+#   # "hashrate",
+#   # "history",
+#   # "workers",
+#   "shareratehistory",
+#   NULL
+# )
+#
+# for (at in tags) {
+#   url <- paste0("https://api.nanopool.org/v1/xmr/",
+#                 at, "/",
+#                 ADDRESS)
+#   cat(url, "\n")
+#   res <- GET(url, accept_json())
+#   print(content(res, type="application/json"))
+# }
+
+## read json from site
+read_tag <- function(tag) {
+  content(
+    GET(
+      paste0("https://api.nanopool.org/v1/xmr/",
+             tag,
+             "/",
+             ADDRESS),
+      accept_json()),
+    type = "application/json")
+}
+
+## read json and make a table or variable
+read_data <- function(tag) {
+  res <- read_tag(tag)
+  if (res[["status"]]) {
+    cat("Got:", tag, "\n")
+    if (is.list(res[["data"]])) {
+      DT_tmp           <- data.frame(t(list2DF(res[["data"]])))
+      names(DT_tmp)    <- names(res[["data"]][[1]])
+      rownames(DT_tmp) <- NULL
+      tblname          <<- paste0(tag, "_DT")
+      assign(tblname, DT_tmp, envir = .GlobalEnv)
+      cat("Created:", tblname, "\n")
+    } else {
+      tblname          <<- paste0(tag, "_Value")
+      assign(tblname, res[["data"]], envir = .GlobalEnv)
+      cat("Created:", tblname, "\n")
+    }
+  } else {
+    cat("No response for", tag, "\n")
+  }
+}
+
+##  Parse and store what we care for  ---------------
+
+try({
+  filest <- paste0(storagePre, "Status.Rds")
+  read_data("balance")
+  read_data("hashrate")
+  if (!exists("balance_Value"))  {balance_Value <- NA}
+  if (!exists("hashrate_Value")) {hashrate_Value <- NA}
+  current <- data.frame(Date     = Sys.time(),
+                        Balande  = balance_Value,
+                        Hashrate = hashrate_Value)
+  if (file.exists(filest)) {
+    STATUS <- readRDS(filest)
+    STATUS <- rbind(STATUS, current)
+  } else {
+    STATUS <- current
+  }
+  saveRDS(STATUS, filest)
+})
+
+
+try({
+  filest <- paste0(storagePre, "Hashratechart.Rds")
+  read_data(                   "hashratechart")
+  if (file.exists(filest)) {
+    STATUS <- readRDS(filest)
+    STATUS <- rbind(STATUS,     hashratechart_DT)
+  } else {
+    STATUS <-                   hashratechart_DT
+  }
+  STATUS <- unique(STATUS)
+  saveRDS(STATUS, filest)
+})
+
+
+try({
+  filest <- paste0(storagePre, "History.Rds")
+  read_data(                   "history")
+  if (file.exists(filest)) {
+    STATUS <- readRDS(filest)
+    STATUS <- rbind(STATUS,     history_DT)
+  } else {
+    STATUS <-                   history_DT
+  }
+  STATUS <- unique(STATUS)
+  saveRDS(STATUS, filest)
+})
+
+
+try({
+  filest <- paste0(storagePre, "Workers.Rds")
+  read_data(                   "workers")
+  if (file.exists(filest)) {
+    STATUS <- readRDS(filest)
+    STATUS <- rbind(STATUS,     workers_DT)
+  } else {
+    STATUS <-                   workers_DT
+  }
+  STATUS <- unique(STATUS)
+  saveRDS(STATUS, filest)
+})
+
+
+try({
+  filest <- paste0(storagePre, "Shareratehistory.Rds")
+  read_data(                   "shareratehistory")
+  if (file.exists(filest)) {
+    STATUS <- readRDS(filest)
+    STATUS <- rbind(STATUS,     shareratehistory_DT)
+  } else {
+    STATUS <-                   shareratehistory_DT
+  }
+  STATUS <- unique(STATUS)
+  saveRDS(STATUS, filest)
+})
+
+#+ include=T, echo=F, results="asis"
+tac <- Sys.time()
+cat(sprintf("**END** %s %s@%s %s %f mins\n\n", Sys.time(), Sys.info()["login"],
+            Sys.info()["nodename"], basename(Script.Name), difftime(tac,tic,units = "mins")))
