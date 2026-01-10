cd19f58e95cca117499d42487878269a767dd67c
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-01-10 12:07:13 +0200

    Commit 2026-01-10 12:07

--- session/session_startup.sh
+++ session/session_startup.sh
@@ -19,35 +19,23 @@ kill_run () {
     setsid     "$@" &
 }
 
-
 # kill "$(ps -ef | grep "languagetool.jar" | grep -v grep | awk '{print $2}')"
 # nohup java -jar ~/PROGRAMS/LanguageTool-4.1/languagetool.jar  --tray &
-
-
-## to have iso date in pcmanfm
-## have to do dpkg-reconfigure and activate en_DK.UTF-8
-export LC_TIME=en_DK.UTF-8
-export _JAVA_AWT_WM_NONREPARENTING=1                                                             [7:36:07]
-
-## use numbers on num keyboard
-numlockx on
 
 
 ## -------------------
 ## Application Daemons
 ## -------------------
 
-
 # for java
 wmname LG3D
-
 
 kill_run  nm-tray
 kill_run  nm-applet
 kill_run  system-config-printer-applet
 kill_run  dunst
 kill_run  kdeconnect-indicator
-kill_run  volumeicon
+# kill_run  volumeicon
 # kill_run  $HOME/CODE/conky/scripts/top_ps.sh
 
 if [[ $WM_NAME = "bspwm" ]]; then
@@ -59,7 +47,6 @@ else
 fi
 
 # kill_run  "python3 $HOME/PROGRAMS/noisy/noisy.py --config $HOME/PROGRAMS/noisy/config.json"
-
 
 ## restart sycnthing server
 killall -9 syncthing
@@ -73,14 +60,13 @@ setsid syncthing -config="$HOME/.config/syncthing_$(hostname)" \
 ## restart applet
 kill_run syncthing-gtk --minimized --home "$HOME/.config/syncthing_$(hostname)"
 
-
 if [[ $(hostname) == "crane" ]]; then
     ## make menu key for logitek keyboard
     /usr/bin/xmodmap -e "keysym ISO_Level3_Shift = Menu"
 fi
 
+## use numbers on num keyboard
 numlockx on
-
 
 # killall -s 9 evolution
 # nohup $HOME/BASH/deamonize.sh "evolution -c calendar --name Evolution_i3" &
@@ -93,45 +79,38 @@ GTK_THEME=Adwaita:dark setsid "$HOME/.nix-profile/bin/thunderbird" &
 # pkill evolution
 # nohup $HOME/BASH/deamonize.sh "evolution" &
 
-
-# killall -s 9 stalonetray
-# nohup $daemonize "stalonetray --sticky -bg black --window-strut right --icon-size 19 --grow-gravity W --skip-taskbar --geometry  4x1-650-6 --icon-gravity W --kludges fix_window_pos" &
-
-
 ## a new toy
 ## this may break conky
 # "$HOME/BASH/notification_log.sh" "$HOME/LOGs/SYSTEM_LOGS/Notification_$(hostname).log" &
-
 
 ## start conky
 ## update backgrounds on all screens
 export DISPLAY=$display; "$HOME/CODE/conky/scripts/update_background.sh" &
 
 # export DISPLAY=0 && $HOME/BASH/STARTUP/conky_chooser.sh
-export DISPLAY=$display; "$HOME/CODE/conky/conky_choose_bigger.sh" &
-
+# export DISPLAY=$display; "$HOME/CODE/conky/conky_choose_bigger.sh" &
 
 ## start all conky cron scripts and truncate status logs
 "$HOME/CODE/conky/crons/run_all_crons.sh" &
 
-## test polybar
-killall polybar
-xrandr --listactivemonitors | sed 's/^.* //' | sed 1d | while read screen; do
-    export MONITOR=$screen
+# ## test polybar
+# killall polybar
+# xrandr --listactivemonitors | sed 's/^.* //' | sed 1d | while read screen; do
+#     export MONITOR=$screen
+# 
+#     ## use the right bar
+#     if [[ $WM_NAME = "i3" ]]; then
+#         polybar -c $HOME/.config/polybar/config -r i3bar &
+#     elif [[ $WM_NAME = "bspwm" ]]; then
+#         polybar -c $HOME/.config/polybar/config -r bspwmbar &
+#     else
+#         polybar -c $HOME/.config/polybar/config -r example &
+#     fi
+# done
 
-    ## use the right bar
-    if [[ $WM_NAME = "i3" ]]; then
-        polybar -c $HOME/.config/polybar/config -r i3bar &
-    elif [[ $WM_NAME = "bspwm" ]]; then
-        polybar -c $HOME/.config/polybar/config -r bspwmbar &
-    else
-        polybar -c $HOME/.config/polybar/config -r example &
-    fi
-done
-
-## i3 auto layout
-if [[ $WM_NAME = "i3" ]]; then
-    setsid $HOME/.config/i3/i3-automatic-layout.py &
-fi
+# ## i3 auto layout
+# if [[ $WM_NAME = "i3" ]]; then
+#     setsid $HOME/.config/i3/i3-automatic-layout.py &
+# fi
 
 exit 0
