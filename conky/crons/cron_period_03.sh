8409e8bccfa9a6a9c3692284fded7f65a2182f2f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-01-29 09:07:24 +0200

    Commit 2026-01-29 09:07

--- conky/crons/cron_period_03.sh
+++ conky/crons/cron_period_03.sh
@@ -3,22 +3,23 @@
 #### Run conky scripts every 3 minutes
 
 ##  External kill switch  ###########################################
-killfile="/dev/shm/KILL_SWITCH/$(basename "$0")"
-[[ -f "$killfile" ]] && echo && echo "KILL SWITCH: $killfile !!!" && exit 99
+# killfile="/dev/shm/KILL_SWITCH/$(basename "$0")"
+# [[ -f "$killfile" ]] && echo && echo "KILL SWITCH: $killfile !!!" && exit 99
 
 ##  Dot no run headless  ############################################
-xsessions="$(w | grep -o " :[0-9]\+ " | sort -u | wc -l)"
-if [[ $xsessions -gt 0 ]]; then
-    echo "Display exists $xsessions"
-else
-    echo "No X server at \$DISPLAY [$DISPLAY] $xsessions" >&2
-#     exit 0
-fi
+# xsessions="$(w | grep -o " :[0-9]\+ " | sort -u | wc -l)"
+# if [[ $xsessions -gt 0 ]]; then
+#     echo "Display exists $xsessions"
+# else
+#     echo "No X server at \$DISPLAY [$DISPLAY] $xsessions" >&2
+# #     exit 0
+# fi
 
 ##  Start  Watchdog for script  ------------------------------------------------
-mainpid=$$
-(sleep $((60*3)); kill -9 $mainpid) &
-watchdogpid=$!
+## TODO use trap
+# mainpid=$$
+# (sleep $((60*3)); kill -9 $mainpid) &
+# watchdogpid=$!
 
 ##  INIT  ----------------------------------------------------------------------
 mkdir -p "/dev/shm/CONKY"
@@ -37,5 +38,5 @@ set +e
 # wait "${pids[@]}"; pids=()
 echo
 echo "Took $SECONDS seconds for $0 to complete"
-kill "$watchdogpid"
+# kill "$watchdogpid"
 exit 0
