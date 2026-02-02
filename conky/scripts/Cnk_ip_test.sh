4a966c35a8e4ad30134c4a3a5a97d755d7f3c3ef
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-02 14:07:08 +0200

    Commit 2026-02-02 14:07

--- conky/scripts/Cnk_ip_test.sh
+++ conky/scripts/Cnk_ip_test.sh
@@ -3,19 +3,21 @@
 #### Ping my hosts and print for conky
 
 ## Host list to check, keep all columns filled with something
-##  Name   tinc          tailscale        Static IP
+##  Name   tinc           tailscale        Static IP
 hosts=(
-  "crane   10.12.12.1    crane.lobster-atlas.ts.net  X.X          "
-  "blue    10.12.12.2    100.119.161.83              X.X          "
-  "kostas  10.12.12.4    X.X                         X.X          "
-  "sagan   10.12.12.5    100.101.191.106             155.207.9.214"
-  "tyler   10.12.12.6    100.119.4.3                 X.X          "
-  "victor  10.12.12.7    X.X                         X.X          "
-  "a34     10.12.12.9    100.120.166.39              X.X          "
-  "door    10.12.12.10   X.X                         X.X          "
-  "yperos  10.12.12.101  100.64.199.88               155.207.10.70"
-  "Y       10.12.14.97   X.X                         X.X          "
-  "mumra   X.X           100.108.41.27               X.X          "
+  "crane   10.12.12.1     crane.lobster-atlas.ts.net  X.X          "
+  "blue    10.12.12.2     100.119.161.83              X.X          "
+  "kostas  10.12.12.4     X.X                         X.X          "
+  "sagan   10.12.12.5     100.101.191.106             155.207.9.214"
+  "tyler   10.12.12.6     100.119.4.3                 X.X          "
+  "victor  10.12.12.7     X.X                         X.X          "
+  "a34     10.12.12.9     100.120.166.39              X.X          "
+  "door    10.12.12.10    X.X                         X.X          "
+  "yperos  10.12.12.101   100.64.199.88               155.207.10.70"
+  "Y       10.12.14.97    X.X                         X.X          "
+  "mumra   X.X            100.108.41.27               X.X          "
+  "gpu-2   10.201.47.102  100.126.97.18               X.X          "
+  "haos    X.X             100.89.58.48               X.X          "
 )
 
 ## Check all hosts ips
