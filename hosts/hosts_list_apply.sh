30928c00e3f71f822df4053e503c490ea84a5000
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-08 18:07:05 +0200

    Commit 2026-02-08 18:07

--- hosts/hosts_list_apply.sh
+++ hosts/hosts_list_apply.sh
@@ -104,22 +104,22 @@ date +"# Created: %F %R " > "$mainfile"
 echo ""                   >> "$mainfile"
 echo "
 127.0.0.1 localhost
+127.0.0.1 $(hostname)
 127.0.0.1 localhost.localdomain
-127.0.0.1 local
-255.255.255.255 broadcasthost
-::1 localhost
-::1 ip6-localhost
-::1 ip6-loopback
-fe80::1%lo0 localhost
-ff00::0 ip6-localnet
-ff00::0 ip6-mcastprefix
-ff02::1 ip6-allnodes
-ff02::2 ip6-allrouters
-ff02::3 ip6-allhosts
+# 255.255.255.255 broadcasthost
+# ::1 localhost
+# ::1 ip6-localhost
+# ::1 ip6-loopback
+# fe80::1%lo0 localhost
+# ff00::0 ip6-localnet
+# ff00::0 ip6-mcastprefix
+# ff02::1 ip6-allnodes
+# ff02::2 ip6-allrouters
+# ff02::3 ip6-allhosts
 
 "                          >> "$mainfile"
 
-## The final file to use 
+## The final file to use
 cat "$maintemp" >> "$mainfile"
 
 size="$(stat -c%s $mainfile)"
