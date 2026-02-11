6848e7fdc5dc09c48e6932b6d7f8141c3c888ee4
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-11 21:07:06 +0200

    Commit 2026-02-11 21:07

---
+++ data_streams/DEFINITIONS.R
@@ -0,0 +1,54 @@
+
+## Data paths
+
+## DTK values form BoG scraped and used
+dtk_fl      <- "~/CODE/fi_analysis/data/BoG_DTK.Rds"
+
+## BGF Euro Income Fixed Maturity Bond Fund 2029 over time
+bon_fl       <- "~/CODE/fi_analysis/data/bonBR.Rds"
+
+## Accounts status from emails
+lastfl       <- "~/CODE/fi_analysis/data/lastA.Rds"
+
+## Accounts status from mmex
+mmexfl       <- "~/CODE/fi_analysis/data/lastmmex.Rds"
+mmexdb       <- "~/MISC/a34_export/MMEX/main.mmb"
+
+## Peiraios master file g values
+peir_ma      <-  "~/CODE/fi_analysis/data/peiraios_gold.Rds"
+
+## Mailed account records from noa
+expfile      <- "~/DATA/Other/Accounts/Mailed_accounts.Rds"
+reportsfile  <- "~/DATA/Other/Accounts/Mailed_csv_reports.Rds"
+
+## Prefix of temporary Mailed account records
+datastorepre <- "~/DATA_RAW/Other/Accounts_Alerts_Mail_P_NOA/PT_"
+
+## Commodity values storage
+comod_fl     <- "~/CODE/fi_analysis/data/Comod.Rds"
+
+## Export of mmex state
+mmexfla      <- "~/CODE/fi_analysis/data/allmmex.Rds"
+
+## Account names form MMEX
+mmexnames    <- "~/CODE/fi_analysis/data/MMEX_account_names.Rds"
+
+## Current commodity status
+cass_fl      <- "~/CODE/fi_analysis/data/test_g_cur.Rds"
+
+## Current commodity assets
+ass_fl       <- "~/Formal/Asset/test_g.ods"
+
+## Current commodity prices from BOG
+bult_fl      <- "~/CODE/fi_analysis/data/BoG_Bulletin_Gold.Rds"
+
+## Repo of csv files
+winbcsvdir   <- "~/DATA_RAW/Other/Winbank_files"
+
+## Mailed account reports from noa
+expfilecsv   <- "~/DATA/Other/Accounts/Mailed_accounts_csv.Rds"
+
+
+## gpx folder
+gpx_dir       <- "~/MISC/a34_export/gpxlog/"
+last_gpx_mins <- 30
---
+++ data_streams/helpers/fn_get_gpx_last_location.R
@@ -0,0 +1,65 @@
+
+require(sf,        quietly = TRUE)
+require(lubridate, quietly = TRUE)
+require(dplyr,     quietly = TRUE)
+
+#' Get a robust last location from a gpx file
+#'
+#' @param gpx_file     Path of a gpx file
+#' @param last_minutes Include last n minutes in the calculations
+#' @param min_points   Include last n points in the calculations
+#'
+#' @returns            A sf object with statistical difined lat, lon, elevation
+#' @export
+#'
+get_gpx_last_location <- function(gpx_file, last_minutes, min_points = 5) {
+  # Read GPX file
+  gpx <- sf::st_read(gpx_file, layer = "track_points", quiet = TRUE)
+
+  # Extract time and coordinates
+  gpx <- gpx %>%
+    mutate(
+      time      = ymd_hms(time),  # Convert to datetime
+      lat       = st_coordinates(.)[, "Y"],
+      lon       = st_coordinates(.)[, "X"],
+      elevation = as.numeric(ele)
+    ) %>%
+    select(time, lat, lon, elevation)
+
+  # Get current time (or most recent time in data)
+  current_time <- max(gpx$time, na.rm = TRUE)
+
+  # Filter for last 10 minutes
+  ten_min_ago  <- current_time - minutes(last_minutes)
+  last_minutes <- gpx %>%
+    filter(time >= ten_min_ago)
+
+  # get last points regardless of time
+  last_by_count <- gpx %>%
+    arrange(desc(time)) %>%
+    slice_head(n = min_points)
+
+  last_points <- bind_rows(last_minutes, last_by_count) %>%
+    distinct() %>%
+    arrange(time)
+
+
+  # Calculate mean location
+  mean_location <- last_points %>%
+    summarise(
+      mean_lat        = mean(lat, na.rm = TRUE),
+      mean_lon        = mean(lon, na.rm = TRUE),
+      median_lat      = median(lat, na.rm = TRUE),
+      median_lon      = median(lon, na.rm = TRUE),
+      start_time      = min(time, na.rm = TRUE),
+      end_time        = max(time, na.rm = TRUE),
+      mean_time       = mean(time, na.rm = TRUE),
+      mean_altitude   = mean(elevation, na.rm = TRUE),
+      median_altitude = median(elevation, na.rm = TRUE),
+      min_altitude    = min(elevation, na.rm = TRUE),
+      max_altitude    = max(elevation, na.rm = TRUE),
+      n_points        = n()
+    )
+
+  return(mean_location)
+}
