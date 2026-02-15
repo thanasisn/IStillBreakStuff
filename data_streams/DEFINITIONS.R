1985b5d97a8f9a1330a255486d1453b08653f367
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-15 09:07:07 +0200

    Commit 2026-02-15 09:07

--- data_streams/DEFINITIONS.R
+++ data_streams/DEFINITIONS.R
@@ -2,20 +2,20 @@
 ##   Fi analysis   ###############
 
 ## DTK values form BoG scraped and used
-dtk_fl          <- "~/CODE/fi_analysis/data/BoG_DTK.Rds"
+dtk_fl          <- "~/CODE/data_streams/fi_analysis/data/BoG_DTK.Rds"
 
 ## BGF Euro Income Fixed Maturity Bond Fund 2029 over time
-bon_fl          <- "~/CODE/fi_analysis/data/bonBR.Rds"
+bon_fl          <- "~/CODE/data_streams/fi_analysis/data/bonBR.Rds"
 
 ## Accounts status from emails
-lastfl          <- "~/CODE/fi_analysis/data/lastA.Rds"
+lastfl          <- "~/CODE/data_streams/fi_analysis/data/lastA.Rds"
 
 ## Accounts status from mmex
-mmexfl          <- "~/CODE/fi_analysis/data/lastmmex.Rds"
+mmexfl          <- "~/CODE/data_streams/fi_analysis/data/lastmmex.Rds"
 mmexdb          <- "~/MISC/a34_export/MMEX/main.mmb"
 
 ## Peiraios master file g values
-peir_ma         <-  "~/CODE/fi_analysis/data/peiraios_gold.Rds"
+peir_ma         <-  "~/CODE/data_streams/fi_analysis/data/peiraios_gold.Rds"
 
 ## Mailed account records from noa
 expfile         <- "~/DATA/Other/Accounts/Mailed_accounts.Rds"
@@ -25,28 +25,38 @@ reportsfile     <- "~/DATA/Other/Accounts/Mailed_csv_reports.Rds"
 datastorepre    <- "~/DATA_RAW/Other/Accounts_Alerts_Mail_P_NOA/PT_"
 
 ## Commodity values storage
-comod_fl        <- "~/CODE/fi_analysis/data/Comod.Rds"
+comod_fl        <- "~/CODE/data_streams/fi_analysis/data/Comod.Rds"
 
 ## Export of mmex state
-mmexfla         <- "~/CODE/fi_analysis/data/allmmex.Rds"
+mmexfla         <- "~/CODE/data_streams/fi_analysis/data/allmmex.Rds"
 
 ## Account names form MMEX
-mmexnames       <- "~/CODE/fi_analysis/data/MMEX_account_names.Rds"
+mmexnames       <- "~/CODE/data_streams/fi_analysis/data/MMEX_account_names.Rds"
 
 ## Current commodity status
-cass_fl         <- "~/CODE/fi_analysis/data/test_g_cur.Rds"
+cass_fl         <- "~/CODE/data_streams/fi_analysis/data/test_g_cur.Rds"
 
 ## Current commodity assets
 ass_fl          <- "~/Formal/Asset/test_g.ods"
 
 ## Current commodity prices from BOG
-bult_fl         <- "~/CODE/fi_analysis/data/BoG_Bulletin_Gold.Rds"
+bult_fl         <- "~/CODE/data_streams/fi_analysis/data/BoG_Bulletin_Gold.Rds"
 
 ## Repo of csv files
 winbcsvdir      <- "~/DATA_RAW/Other/Winbank_files"
 
 ## Mailed account reports from noa
 expfilecsv      <- "~/DATA/Other/Accounts/Mailed_accounts_csv.Rds"
+
+## Store some data
+entoka_data_fl  <- "~/CODE/data_streams/fi_analysis/data/entoka_omologa.Rds"
+
+## Store values from peiraios
+fi_data_dir     <- "~/CODE/data_streams/fi_analysis/data/"
+peir_g_prefix   <- "~/CODE/data_streams/fi_analysis/data/peiraios_gold"
+
+## Store tsig data
+tsig_data_fl <- "~/CODE/data_streams/fi_analysis/data/tsig.Rds"
 
 
 ##   Weather   #############
