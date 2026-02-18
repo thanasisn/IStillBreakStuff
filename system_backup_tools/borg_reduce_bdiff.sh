392704c575f0133262807663abd097eb956809cc
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-18 13:07:07 +0200

    Commit 2026-02-18 13:07

--- system_backup_tools/borg_reduce_bdiff.sh
+++ system_backup_tools/borg_reduce_bdiff.sh
@@ -14,23 +14,22 @@ fi
 
 echo "Lines Initial         :  $(wc -l "$file" | cut -d' ' -f1)"
 
-## Remove .git repo entries
-sed -i '/\/.git\//d'      "$file"
-sed -i '/\/.dotfiles\//d' "$file"
-echo "Without git entries    :  $(wc -l "$file" | cut -d' ' -f1)"
-
-## Remove ctime only changes
-sed -i '/\[ctime:/d' "$file"
-echo "Without ctimes changes:  $(wc -l "$file" | cut -d' ' -f1)"
-
-## Remove ctime edits changes
-sed -i '/\[ctime:/d' "$file"
-echo "Without ctimes edits  :  $(wc -l "$file" | cut -d' ' -f1)"
-
-## Remove PROGRAMS changes
-sed -i '/\/PROGRAMS\//d' "$file"
-echo "Without PROGRAMS edits:  $(wc -l "$file" | cut -d' ' -f1)"
-
+# ## Remove .git repo entries
+# sed -i '/\/.git\//d'      "$file"
+# sed -i '/\/.dotfiles\//d' "$file"
+# echo "Without git entries    :  $(wc -l "$file" | cut -d' ' -f1)"
+# 
+# ## Remove ctime only changes
+# sed -i '/\[ctime:/d' "$file"
+# echo "Without ctimes changes:  $(wc -l "$file" | cut -d' ' -f1)"
+# 
+# ## Remove ctime edits changes
+# sed -i '/\[ctime:/d' "$file"
+# echo "Without ctimes edits  :  $(wc -l "$file" | cut -d' ' -f1)"
+# 
+# ## Remove PROGRAMS changes
+# sed -i '/\/PROGRAMS\//d' "$file"
+# echo "Without PROGRAMS edits:  $(wc -l "$file" | cut -d' ' -f1)"
 
 
 ## Open file for inspection
