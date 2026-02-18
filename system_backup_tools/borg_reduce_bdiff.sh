32c126613625cbe06a3c46a7bf26f0af37d6836f
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-18 12:07:05 +0200

    Commit 2026-02-18 12:07

--- system_backup_tools/borg_reduce_bdiff.sh
+++ system_backup_tools/borg_reduce_bdiff.sh
@@ -17,21 +17,30 @@ echo "Lines Initial         :  $(wc -l "$file" | cut -d' ' -f1)"
 ## Remove .git repo entries
 sed -i '/\/.git\//d'      "$file"
 sed -i '/\/.dotfiles\//d' "$file"
-echo "Without git enties    :  $(wc -l "$file" | cut -d' ' -f1)"
+echo "Without git entries    :  $(wc -l "$file" | cut -d' ' -f1)"
 
 ## Remove ctime only changes
-sed -i '/^\[ctime/d' "$file"
+sed -i '/\[ctime:/d' "$file"
 echo "Without ctimes changes:  $(wc -l "$file" | cut -d' ' -f1)"
 
 ## Remove ctime edits changes
-sed -i '/\[ctime/d' "$file"
+sed -i '/\[ctime:/d' "$file"
 echo "Without ctimes edits  :  $(wc -l "$file" | cut -d' ' -f1)"
 
 ## Remove PROGRAMS changes
 sed -i '/\/PROGRAMS\//d' "$file"
 echo "Without PROGRAMS edits:  $(wc -l "$file" | cut -d' ' -f1)"
 
+
+
 ## Open file for inspection
-less "$file"
+#less "$file"
+
+sed \
+  -e '/\/.git\//d' \
+  -e '/\/.dotfiles\//d' \
+  -e '/\[ctime/d' \
+  -e '/\/PROGRAMS\//d' \
+  "$file" | less
 
 exit 0
