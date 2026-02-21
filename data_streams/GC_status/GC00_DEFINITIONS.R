25042fad7751dfae561b756d9da2be4b4dda2d75
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-02-21 18:07:10 +0200

    Commit 2026-02-21 18:07

--- data_streams/GC_status/GC00_DEFINITIONS.R
+++ data_streams/GC_status/GC00_DEFINITIONS.R
@@ -7,9 +7,13 @@ gccache   <- "~/TRAIN/GoldenCheetah/Athan/cache/rideDB.json"
 storagefl <- "~/DATA/Other/GC_json_ride_data_3.Rds"
 
 
-
+##  all data plot
 pdfout1   <- "~/LOGs/training_status/GC_all_variables.pdf"
 pdfout2   <- "~/LOGs/training_status/GC_all_variables_last.pdf"
 LASTDAYS  <- 400
 
 DATA_PLOT_LIMIT <- 10
+
+
+## performance analysis
+daysback    <- 366 * 4
