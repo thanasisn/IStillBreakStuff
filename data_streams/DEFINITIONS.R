6cac1f4a7089668760831fe86abd74753a82da0e
thanasisn <11601717+thanasisn@users.noreply.github.com>
2026-09-07 13:07:18 +0300

    Commit 2026-09-07 13:07

--- data_streams/DEFINITIONS.R
+++ data_streams/DEFINITIONS.R
@@ -67,8 +67,8 @@ taplog_fl       <- "~/DATA_RAW/Other/TapLog.Rds"
 efc30255_fl     <- "~/CODE/data_streams/fi_analysis/data/efc30155_cur.Rds"
 
 ## Export BNP Paribas
-BNPFlexinav_fl     <- "~/CODE/data_streams/fi_analysis/data/BNPParibasFlexinav.Rds"
-BNPFlexidiv_fl     <- "~/CODE/data_streams/fi_analysis/data/BNPParibasFlexidiv.Rds"
+BNPFlexinav_fl     <- "~/CODE/data_streams/fi_analysis/data/BNPParibasFlexiNAV.Rds"
+BNPFlexidiv_fl     <- "~/CODE/data_streams/fi_analysis/data/BNPParibasFlexiDIV.Rds"
 
 
 ##   Weather   #############
